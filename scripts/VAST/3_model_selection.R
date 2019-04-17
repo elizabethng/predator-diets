c351fc90b296b619b3103db2c80b22be9ca849ac
Elizabeth Ng <elng@uw.edu>
2019-04-17 10:57:49 -0700

    Update 3_model_selection.R

--- scripts/VAST/3_model_selection.R
+++ scripts/VAST/3_model_selection.R
@@ -1,26 +1,36 @@
+# Load VAST wrapper for model selection
 source("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets/scripts/VAST/wrapper_vast.R")
 
 # Atlantic Cod
-test = ss_wrapper_vast(species_num = 4, 
+ss_wrapper_vast(species_num = 4, 
                 file_prefix = "mod_sel_DG_", 
                 Save_output = FALSE, 
                 ObsModel = c(2,0))
 
-t2 = ss_wrapper_vast(species_num = 4, 
+ss_wrapper_vast(species_num = 4, 
                        file_prefix = "mod_sel_PLD_", 
                        Save_output = FALSE, 
                        ObsModel = c(2,1))
 
 # Spiny Dogfish
-t3 = ss_wrapper_vast(species_num = 8, 
+ss_wrapper_vast(species_num = 8, 
                        file_prefix = "mod_sel_DG_", 
                        Save_output = FALSE, 
                        ObsModel = c(2,0))
 
-t4 = ss_wrapper_vast(species_num = 8, 
+ss_wrapper_vast(species_num = 8, 
                      file_prefix = "mod_sel_PLD_", 
                      Save_output = FALSE, 
                      ObsModel = c(2,1))
 
 
-# Third species
+# Bluefish
+ss_wrapper_vast(species_num = 13, 
+                file_prefix = "mod_sel_DG_", 
+                Save_output = FALSE, 
+                ObsModel = c(2,0))
+
+ss_wrapper_vast(species_num = 13, 
+                file_prefix = "mod_sel_PLD_", 
+                Save_output = FALSE, 
+                ObsModel = c(2,1))
