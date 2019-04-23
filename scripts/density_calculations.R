ff43b15ac63909f3c9b01fdf85773ceeff13df26
Elizabeth Ng <elng@uw.edu>
2019-04-22 18:09:22 -0700

    Update density_calculations.R

--- scripts/density_calculations.R
+++ scripts/density_calculations.R
@@ -5,16 +5,23 @@ library(here)
 library(VAST)
 
 output_folder = "cod_density_plg_rw"
-
+path_name = paste("output", "VAST", "_runs_for_popdy_meeting", output_folder, sep = "/")
 
 # Load data ---------------------------------------------------------------
 
-load(here("output", "VAST", "_runs_for_popdy_meeting", output_folder, "Save.RData"))
-load(here("output", "VAST", "_runs_for_popdy_meeting", output_folder, "Kmeans-100.RData"))
+
+# load(here(path_name, "Save.RData"))
+load(here(path_name, "Kmeans-100.RData"))
+load(here(path_name, "Record.RData"))
+density = read_csv(here(path_name, "Dens_DF.txt"))
 
 
 
 # Density estimates -------------------------------------------------------
+# x = knots, c = clusters, y = years
+# Save$Report$D_xcy
+
+ggplot(density, aes(E_km, N_km, col = Density)) + facet_wrap(~Year) + geom_point()
 
 
 
@@ -22,9 +29,12 @@ load(here("output", "VAST", "_runs_for_popdy_meeting", output_folder, "Kmeans-10
 # Knot information --------------------------------------------------------
 
 knot_dat = data.frame(Kmeans$centers)
-knot_dat$rel_area = Kmeans$size/sum(Kmeans$size) # for even grid, area is propto #pts in cluster
+knot_dat$rel_area = Kmeans$size/sum(Kmeans$size)
+# for even grid, area is propto #pts in cluster, but might not be even
+knot_dat$area = Record$Spatial_List$PolygonList$a_xl
 
 # how to get knot polygons??
+NN_Extrap = Spatial_List$PolygonList$NN_Extrap
 
 
 # From VAST, piece together things?
