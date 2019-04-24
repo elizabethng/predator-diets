b499c909ce3f12f05bc8660d5d5f8fe397d8513a
Elizabeth Ng <elng@uw.edu>
2019-04-24 14:41:12 -0700

    bunch of quick plotting changes

--- scripts/map_index.R
+++ scripts/map_index.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(here)
+library(gganimate)
 
 
 results_files = c("herring_density_plg_rw", 
@@ -39,12 +40,34 @@ map_dat = left_join(all_dens, all_locations) %>%
 
 # missing 34....
 map_dat[!complete.cases(map_dat),]
-
+map_dat = map_dat[complete.cases(map_dat),]
 
 # Try mapping
-ggplot(filter(map_dat, year == 1973),
+p = ggplot(filter(map_dat, Include == TRUE), 
        aes(x = Lon, y = Lat, color = density_log)) +
-  geom_point()
-  
+  geom_point() +
+  scale_color_viridis_c(
+    option = "inferno", 
+    name = "log(Density)") +
+  borders("world", fill = "grey", colour = "white") +
+  coord_quickmap(xlim = c(-77, -63), ylim = c(37, 47)) +
+  theme(panel.grid.major = element_line(color = "white"),
+        panel.background = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) + 
+  theme(legend.position = "bottom") +
+  labs(title = 'Year: {closest_state}') +
+  transition_states(year,
+                    transition_length = 2,
+                    state_length = 5)
+  # facet_wrap(~year)
+
+anim_save(animation = p, filename = here("test_anim_cod_cons.gif"))
+
+
 
 
--- scripts/predator_consumption_summary.R
+++ scripts/predator_consumption_summary.R
@@ -1,7 +1,11 @@
 # Use dat_preds_all from 1_process_consumption_data.Rmd
 
-dat_preds_all
+library(here)
 
+dat_preds_all = read_rds(here("output", "data_formatted", "dat_preds_all.rds"))
+
+
+# Summarize herring consumption by species --------------------------------
 my_summary = dat_preds_all %>% 
   group_by(pdcomnam) %>%
   summarize(
@@ -31,4 +35,31 @@ ggplot(my_summary, aes(x = pdcomnam, y = total_kg)) +
   ylab("Herring consumption (kg)") + 
   theme(text = element_text(size = 16))
 
+
+# Summarize herring consumption by size -----------------------------------
+
+size_summary = dat_preds_all %>% 
+  filter(pdcomnam == "ATLANTIC COD" | pdcomnam == "SPINY DOGFISH") %>%
+  group_by(sizecat, pdcomnam) %>%
+  summarize(
+    average_g = mean(pyamtw),
+    n_pred = n()) %>%
+  ungroup(sizecat) %>%
+  # mutate(sizecat = factor(sizecat))
+  mutate(sizecat = factor(sizecat, levels = c("S", "M", "L", "XL")))
+  
+
+ggplot(size_summary, aes(x = sizecat, y = average_g)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~pdcomnam) +
+  theme_bw() + 
+  theme(
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.line = element_line(colour = "black")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab(NULL) +
+  ylab("Mean per capita herring consumption (g)") + 
+  theme(text = element_text(size = 16))
 
