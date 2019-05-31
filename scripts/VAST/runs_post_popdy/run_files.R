dac3d65007ab60b42c13cadc8453a9a96c436e39
Elizabeth Ng <elng@uw.edu>
2019-05-31 12:43:27 -0700

    Create run_files.R

---
+++ scripts/VAST/runs_post_popdy/run_files.R
@@ -0,0 +1,5 @@
+source("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets/scripts/VAST/runs_post_popdy/dogfish_consumption_plg_rw_SPRING_A.R")
+rm(ls=list())
+source("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets/scripts/VAST/runs_post_popdy/dogfish_consumption_plg_rw_FALL_A.R")
+
+
