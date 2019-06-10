8286d9114b2f475d02d7c53a29159b342b4d8801
Elizabeth Ng <elng@uw.edu>
2019-06-10 15:37:46 -0700

    Update compare_seasonal_indices.R

--- scripts/VAST/runs_post_popdy/compare_seasonal_indices.R
+++ scripts/VAST/runs_post_popdy/compare_seasonal_indices.R
@@ -4,15 +4,22 @@ library(tidyverse)
 library(here)
 library(gganimate)
 
+
+# Preliminaries -----------------------------------------------------------
+
+
 species_name = c("dogfish", "cod")[1]
+
+Species = switch(species_name,
+  dogfish = "SPINY DOGFISH",
+  cod = "ATLANTIC COD"
+)
+
 
 results_files = paste0(species_name,
                        c("_consumption_plg_rw",
                          "_consumption_plg_rw_FALL_A",
                          "_consumption_plg_rw_SPRING_A"))
-# results_files = paste0(c("_consumption_plg_rw",
-#   "_consumption_plg_rw_trunc_FALL",
-#   "_consumption_plg_rw_trunc_SPRING_B"))
 
 path_name = paste("output", "VAST", "_runs_post_popdy", sep = "/")
 
@@ -21,6 +28,19 @@ z_score = function(dat){
   z = c/sd(dat)
   return(z)
 }
+
+
+
+# Data manipulation -------------------------------------------------------
+
+obs_consumption = read_rds(here("output", "data_formatted", "dat_preds.rds")) %>%
+  pluck(Species) %>%
+  mutate(season_combo = ifelse(season == "SPRING" | season == "WINTER", "spring", "fall")) %>%
+  group_by(season_combo, year) %>%
+  summarize(mean_consumption = mean(pyamtw),
+            tot_consumption = sum(pyamtw))
+obs_consumption$z_consumption = z_score(obs_consumption$tot_consumption)
+
 
 index = read_csv(here(path_name, results_files[1], "Table_for_SS3.csv")) %>%
   mutate(herring_z = z_score(Estimate_metric_tons))
@@ -47,6 +67,9 @@ index_combo = tibble(
            rep("spring", nrow(spring_index))))
 
 
+# Index Plots -------------------------------------------------------------
+
+# Comparison of raw indices (units g)
 p = ggplot(index_combo, aes(Year, est_g, group = type, color = type)) +
   geom_line(size = 1) + 
   theme_bw() + 
@@ -59,6 +82,8 @@ p = ggplot(index_combo, aes(Year, est_g, group = type, color = type)) +
   theme(text = element_text(size = 16))
 plot(p)
 
+
+# Comparison of normalized indices
 p2 = ggplot(index_combo, aes(Year, est_z, group = type, color = type)) +
   geom_line(size = 1) + 
   theme_bw() + 
@@ -72,6 +97,7 @@ p2 = ggplot(index_combo, aes(Year, est_z, group = type, color = type)) +
 plot(p2)
 
 
+# Joint seasonal model looks like an average of the spring and fall models
 test = rbind(index_combo,
              data.frame(Year = index$Year, 
                   est_g = rowMeans(cbind(fall_index$Estimate_metric_tons, spring_index$Estimate_metric_tons)), 
@@ -88,4 +114,49 @@ ggplot(test, aes(Year, est_g, group = type, color = type)) +
     axis.line = element_line(colour = "black")) +
   ylab("Herring index") + 
   theme(text = element_text(size = 16))
+
+
+
+# Comparison to data ------------------------------------------------------
+
+# Comparison of raw indices (units g)
+p = ggplot() +
+  geom_line(
+    data = filter(index_combo, type != "both"),
+    aes(Year, est_g, group = type, color = type),
+    size = 1) + 
+  geom_line(
+    data = obs_consumption, 
+    aes(year, tot_consumption, group = season_combo, color = season_combo),
+    size = 5, alpha = 0.2) +
+  theme_bw() + 
+  theme(
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.line = element_line(colour = "black")) +
+  ylab("Herring index") + 
+  theme(text = element_text(size = 16))
+plot(p)
+
+
+# Comparison of normalized indices
+p = ggplot() +
+  geom_line(
+    data = filter(index_combo, type != "both"),
+    aes(Year, est_z, group = type, color = type),
+    size = 1) + 
+  geom_line(
+    data = obs_consumption, 
+    aes(year, z_consumption, group = season_combo, color = season_combo),
+    size = 5, alpha = 0.2) +
+  theme_bw() + 
+  theme(
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.line = element_line(colour = "black")) +
+  ylab("Herring index") + 
+  theme(text = element_text(size = 16))
+plot(p)
 
