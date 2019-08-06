caf0ce7f1760c4d411b9c7f5a6527595b125308f
Elizabeth Ng <elng@uw.edu>
2019-08-06 16:24:47 -0700

    Update summary-statistics.R

--- scripts/VAST/runs-for-reading-group/summary-statistics.R
+++ scripts/VAST/runs-for-reading-group/summary-statistics.R
@@ -5,13 +5,13 @@ library(here)
 library(VAST)
 library(TMB)
 
-
 Species = c("SILVER HAKE", "RED HAKE", "FOURSPOT FLOUNDER", "ATLANTIC COD", 
             "POLLOCK", "WHITE HAKE", "WINTER SKATE", "SPINY DOGFISH", "SUMMER FLOUNDER", 
             "GOOSEFISH", "THORNY SKATE", "SEA RAVEN", "BLUEFISH", "WEAKFISH")[8]
 
 
-dat = read_rds(here("output", "data_formatted", "dat_preds_all.rds")) %>%
+# Original predator-level with empties ------------------------------------
+dat = read_rds(here("output", "data_formatted", "dat_preds_all_inc_empty.rds")) %>%
   filter(pdcomnam == Species) %>%
   mutate(myseason = ifelse(season == "FALL"|season == "SUMMER", "FALL", "SPRING"))
 
@@ -22,3 +22,34 @@ annual = dat %>% group_by(year, myseason)%>%
 
 ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col()
 ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col() + facet_grid(~myseason)
+
+
+# New predator-level without empties --------------------------------------
+dat = read_rds(here("output", "data_formatted", "dat_preds_all.rds")) %>%
+  filter(pdcomnam == Species)
+
+annual = dat %>% group_by(year, myseason)%>% 
+  summarize(positive = sum(pypres),
+            total = n()) %>%
+  gather("encounter_type", "number", -year, -myseason)
+
+ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col()
+ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col() + facet_grid(~myseason)
+
+
+
+# New pooled-level without empties ----------------------------------------
+dat = read_rds(here("output", "data_formatted", "dat_tows_all.rds")) %>%
+  filter(pdcomnam == Species)
+
+annual = dat %>% group_by(year, myseason)%>% 
+  summarize(present = sum(tot_pypres),
+            total = n()) %>%
+  mutate(absent = total - present) %>%
+  select(-total) %>%
+  gather("encounter_type", "number", -year, -myseason)
+
+ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col()
+ggplot(annual, aes(year, number, fill = encounter_type)) + geom_col() + facet_grid(~myseason)
+
+
