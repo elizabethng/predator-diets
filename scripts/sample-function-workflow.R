55878717b81e965b2af51a9ed5caa8da5d325e1a
Elizabeth Ng <elng@uw.edu>
2019-10-02 16:52:05 -0700

    Add VAST and TMB to package load list
    The compile package is not enough because there are internal functions that are called within VAST::make_model (the compile step) that need the package to be loaded.

--- scripts/sample-function-workflow.R
+++ scripts/sample-function-workflow.R
@@ -1,18 +1,25 @@
 # Sample workflow with new data-processing functions. 
 
-library(tidyverse)
 # library(here)
-library(VAST)
-library(TMB)
+# library(tidyverse)
+# library(VAST)
+# library(TMB)
+library(magrittr)
+library(compiler)
 
-# Alas, things get wonky now with my project structure. 
-locdir <- c("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets")
+Version <- FishStatsUtils::get_latest_version()
 
+locdir <- c("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets") # Alas, things get wonky now with my project structure. 
 source(file.path(locdir, "functions", "process_data.R"))
-source(file.path(locdir, "functions", "strata_limits_subset.R")) # could save as RDS
+source(file.path(locdir, "functions", "run_mod.R"))
+
+# source(file.path(locdir, "configuration-files", "config-file-example.R")) # Christine passes to run_mods function 
 
 raw_data <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_all.rds")) 
-shake <- process_data(raw_data, species = "SILVER HAKE", season = "spring") 
+mydat <- process_data(raw_data, species = "SPINY DOGFISH", season = "both") 
 
-config_file <- config_file
-run_mods()
+config_file <- c(file.path(locdir, "configuration-files", "config-file-example.R"))
+
+run_mod(Data_Geostat = mydat,
+         config_file = config_file,
+         folder_name = "TEST_my_ouput")
