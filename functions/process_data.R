bb60316a255a7ca9a2ac3841611e735ece1e42b4
Elizabeth Ng <elng@uw.edu>
2019-10-23 15:57:32 -0700

    Aggregate consumption by predator in tow
    To account for non-ignorable sampling desing. Add detection of years with 0% consumption and set them to NA. VAST should avoid estimating year effects for those years. Update warning messages, although these will not display when wrapped in run_mod function.

--- functions/process_data.R
+++ functions/process_data.R
@@ -10,9 +10,7 @@
 #' @examples
 #' \dontrun{
 #' raw_data <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_all.rds")) 
-#' test <- process_data(raw_data, 
-#'                   species = "SILVER HAKE",
-#'                   season = "spring")      
+#' test <- process_data(raw_data, species = "SILVER HAKE", season = "spring")      
 #' }
 process_data <- function(dataset__, species, season){
   # Filter data
@@ -22,13 +20,22 @@ process_data <- function(dataset__, species, season){
     species_data <- dplyr::filter(species_data, myseason == "SPRING")
   }else if(tolower(season) == "fall"){
     species_data <- dplyr::filter(species_data, myseason == "FALL")
+  }else if(tolower(season) != "both"){
+    print("Using spring and fall seasons")
   }
   
-  # Select columns
+  # Aggregate data to tow level and add covariate columns
   dat <- species_data %>%
+    dplyr::mutate(size_cat = ifelse(sizecat == "S", -1, 
+                             ifelse(sizecat == "M", 0, 1))) %>%
+    dplyr::group_by(pdcomnam, towid, year, myseason, declat, declon) %>%
+    dplyr::summarize(
+      pyamtw = mean(pyamtw, na.rm = TRUE),
+      pdlen = mean(pdlen, na.rm = TRUE),
+      size_cat = median(size_cat,  na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
     dplyr::mutate(
       int = 1,
-      size_cat = ifelse(sizecat == "S", -1, ifelse(sizecat == "M", 0, 1)),
       pd_len_z = scale(pdlen)[,1],
       pd_len_z_2 = pd_len_z^2) %>%
     dplyr::select(pdcomnam, myseason, pyamtw, year, declat, declon, 
@@ -36,18 +43,32 @@ process_data <- function(dataset__, species, season){
     na.omit()
   
   
-  # Check for missing years and warn
+  # Check for missing years and set to NA
   all_years <- seq(min(dat$year), max(dat$year))
   obs_years <- unique(dat$year)
   missing_years <- all_years[!(all_years %in% obs_years)]
   
   if(length(missing_years) > 0){
-    warning(paste("Missing year(s) detected:", missing_years))
+    warning(paste("Missing year(s) detected:", paste(missing_years, collapse = ","))) # doesn't appear when wrapped in run_mod
+  }
+  
+  # Check for zero biomass years and set to NA
+  yrs_wo_obs <- dat %>% 
+    dplyr::group_by(year) %>%
+    dplyr::summarize(tot_biomass = sum(pyamtw)) %>%
+    dplyr::filter(tot_biomass == 0) %>%
+    dplyr::pull(year)
+  
+  fix_dat <- dat %>%
+    dplyr::mutate(pyamtw = ifelse(year %in% yrs_wo_obs, NA, pyamtw))
+
+  if(length(yrs_wo_obs) > 0){
+    warning(paste("0% consumption year(s) detected:", paste(yrs_wo_obs, collapse = ","))) # doesn't appear when wrapped in run_mod
   }
   
   
   # Format output
-  data_geo <- dat %>%
+  data_geo <- fix_dat %>%
     dplyr::rename(
       species = pdcomnam,
       season = myseason,
