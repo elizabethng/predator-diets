63f9f81db7e4fdcdd727aed53a5099d5d80bc317
Elizabeth Ng <elng@uw.edu>
2019-10-22 17:30:18 -0700

    Remove old code to load data and add top 5 species

--- scripts/sample-function-workflow.R
+++ scripts/sample-function-workflow.R
@@ -16,30 +16,56 @@ library(magrittr)
 #   3. pipe read data directly to process data to generate output
 
 
-
 Version <- FishStatsUtils::get_latest_version()
 gitdir <- c("C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets") # Alas, things get wonky now with my project structure. 
 source(file.path(gitdir, "functions", "process_data.R"))
 source(file.path(gitdir, "functions", "run_mod.R"))
 
-
-# raw_data <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_all.rds")) 
-# mydat <- process_data(raw_data, species = "SPINY DOGFISH", season = "both") 
-
 config_file_loc <- file.path(gitdir, "configuration-files", "config-file-example.R")
 strata_file_loc <- file.path(gitdir, "configuration-files", "strata_limits_subset.R")
 rawdat_file_loc <- here::here("output", "data_formatted", "dat_preds_all.rds")
-output_file_loc <- here::here("TEST") # overall folder where data are stored, output file name generated in run_mod
+output_file_loc <- here::here("TEST")
 
 
-run_mod(species = "ATLANTIC COD",
+run_mod(species = "SPINY DOGFISH",
         season = "both",
-        covar_columns = NA,
+        covar_columns = "none",
         config_file_loc = config_file_loc,
         strata_file_loc = strata_file_loc,
         rawdat_file_loc = rawdat_file_loc,
         output_file_loc = output_file_loc)
 
+run_mod(species = "ATLANTIC COD",
+        season = "both",
+        covar_columns = "none",
+        config_file_loc = config_file_loc,
+        strata_file_loc = strata_file_loc,
+        rawdat_file_loc = rawdat_file_loc,
+        output_file_loc = output_file_loc)
+
+run_mod(species = "WHITE HAKE",
+        season = "both",
+        covar_columns = "none",
+        config_file_loc = config_file_loc,
+        strata_file_loc = strata_file_loc,
+        rawdat_file_loc = rawdat_file_loc,
+        output_file_loc = output_file_loc)
+
+run_mod(species = "GOOSEFISH",
+        season = "both",
+        covar_columns = "none",
+        config_file_loc = config_file_loc,
+        strata_file_loc = strata_file_loc,
+        rawdat_file_loc = rawdat_file_loc,
+        output_file_loc = output_file_loc)
+
+run_mod(species = "SILVER HAKE",
+        season = "both",
+        covar_columns = "none",
+        config_file_loc = config_file_loc,
+        strata_file_loc = strata_file_loc,
+        rawdat_file_loc = rawdat_file_loc,
+        output_file_loc = output_file_loc)
 
 
 
