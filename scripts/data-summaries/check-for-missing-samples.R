05e79eccc2dd9f69bf253376b132a6d8da7ddae4
Elizabeth Ng <elng@uw.edu>
2019-10-23 11:27:00 -0700

    Add script to check sample sizes for pred, season, year

---
+++ scripts/data-summaries/check-for-missing-samples.R
@@ -0,0 +1,92 @@
+# Script to check sample sizes for all the species I plan to use in my models
+
+library(here)
+library(tidyverse)
+
+raw_data <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_all.rds")) 
+
+
+# Check raw data ----------------------------------------------------------
+# Check for years missing observations and years without positive prey presence
+raw_data %>%
+  filter(pdcomnam %in% c("SPINY DOGFISH", "ATLANTIC COD", "WHITE HAKE", "GOOSEFISH", "SILVER HAKE")) %>%
+  group_by(pdcomnam, myseason, year) %>%
+  summarize(
+    pypres = sum(pyamtw),
+    n = n()
+    ) %>%
+  pivot_longer(-c(pdcomnam, myseason, year), names_to = "type", values_to = "value") %>%
+  ggplot(aes(year, value, group = type, fill = type)) +
+  geom_col(position = "dodge") +
+  facet_grid(myseason ~ pdcomnam)
+
+
+raw_data %>%
+  filter(pdcomnam %in% c("SPINY DOGFISH", "ATLANTIC COD", "WHITE HAKE", "GOOSEFISH", "SILVER HAKE")) %>%
+  group_by(pdcomnam, myseason, year) %>%
+  summarize(
+    pypres = sum(pyamtw) > 0,
+    n = n()
+  ) %>%
+  ggplot(aes(year, n, fill = pypres)) +
+  geom_col() +
+  facet_grid(myseason ~ pdcomnam)
+
+# Based on this, I'll have to truncate some years off of goosefish, silver hake, and white hake
+# I definitely won't be able to a spring model for white hake. 
+
+# However, if I take Jim's suggestion and average by tows, then I think that will
+# fix a lot of things.
+
+
+# Check tow-level averages ------------------------------------------------
+
+# towid should be unique across seasons, but double check (evaluates to 0 if ok)
+raw_data %>% 
+  group_by(towid) %>% 
+  summarize(n_seasons = length(levels(as.factor(myseason)))) %>%
+  mutate(too_many = ifelse(n_seasons > 1, TRUE, FALSE)) %>%
+  summarize(n_too_many = sum(too_many))
+  
+# Check the unique number of of predator x tow combinations
+raw_data %>%
+  group_by(pdcomnam, towid) %>%
+  summarize(n = n()) %>%
+  nrow()
+
+# Get average per tow values (of prey amount and covariates...)
+tow_avg_dat <- raw_data %>%
+  mutate(size_cat = ifelse(sizecat == "S", -1, 
+                           ifelse(sizecat == "M", 0, 1))) %>%
+  group_by(pdcomnam, towid, year, myseason, declat, declon) %>%
+  summarize(
+    pyamtw_mean = mean(pyamtw, na.rm = TRUE),
+    pdlen_mean = mean(pdlen, na.rm = TRUE),
+    # pdwgt_mean = mean(pdwgt, na.rm = TRUE),
+    sizecat_median = median(size_cat,  na.rm = TRUE))
+
+nrow(tow_avg_dat) # good, matches above
+
+# Check missingness
+tow_avg_dat$pyamtw_mean %>% is.na() %>% sum()
+tow_avg_dat$pdlen_mean %>% is.na() %>% sum()
+# tow_avg_dat$pdwgt_mean %>% is.na() %>% sum() # that's why I left these out
+tow_avg_dat$sizecat_median %>% is.na() %>% sum()
+
+tow_avg_dat %>%
+  filter(pdcomnam %in% c("SPINY DOGFISH", "ATLANTIC COD", "WHITE HAKE", "GOOSEFISH", "SILVER HAKE")) %>%
+  group_by(pdcomnam, myseason, year) %>%
+  summarize(
+    pypres = sum(pyamtw_mean) > 0,
+    n = n()
+  ) %>%
+  ggplot(aes(year, n, fill = pypres)) +
+  geom_col() +
+  facet_grid(myseason ~ pdcomnam)
+
+# Now that I can visualize this, how should I deal with the missing data?
+# Especially for the seasonal models, where things ger very sparse?
+# It's also tough, becuase the early years are interesting becasue herring
+# abundance was low. 
+
+# A good option could be to fix those years to zero, need to check how to do that.
