7629a1c94afe19a434b497d1086a0285564b6a40
Elizabeth Ng <elng@uw.edu>
2019-10-29 17:36:04 -0700

    Add check for a subset of years, correct UTM from km

--- scripts/data-summaries/map-tows-stomachs-and-knots.R
+++ scripts/data-summaries/map-tows-stomachs-and-knots.R
@@ -5,9 +5,18 @@
 # Also potentially include
 #  * sample sizes for each?
 
+# How do I plan to use this as a function?
+# Give it a folder with config-file name
+#  * extract the season from the config-file name
+# 
+
 library(here)
 library(tidyverse)
 library(sf)
+
+output_folder <- "new_test"
+config_file_name <- "spiny-dogfish_season-spring_covar-NA_lognm-pl-independent-years-no2spatial"
+
 
 map_theme <- function(...){
   theme(panel.grid.major = element_line(color = "white"),
@@ -18,14 +27,13 @@ map_theme <- function(...){
         axis.title.y = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
-        legend.position = c(0.3, 0.12),
-        legend.direction = "horizontal", 
+        # legend.position = c(0.9, 0.1), # c(0.3, 0.12),
+        # legend.direction = "horizontal", 
         strip.background = element_blank(), 
         text = element_text(size = 13),
         ...)
 }
 
-# new_test/spiny-dogfish_season-spring_covar-NA_lognm-pl-independent-years-no2spatial
 
 # Get all trawl locations
 all_trawls <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_all.rds")) %>%
@@ -37,54 +45,78 @@ all_trawls <- readr::read_rds(here::here("output", "data_formatted", "dat_preds_
   ) %>%
   mutate(
     Season = tolower(Season)
-  )
+  ) %>%
+  st_as_sf(coords = c("declon", "declat"), crs = 4326)
 
 # utms <- all_trawls %>%
-#   st_as_sf(coords = c("declon", "declat"), crs = 4326) %>%
 #   st_transform(crs = 32619) %>%
 #   st_coordinates()
-# 
-# all_trawls <- all_trawls %>%
-#   mutate(
-#     "Easting (km)" = utms[, 1],
-#     "Northing (km)" = utms[, 2]
-#   )
+
 
 # Get non-empty stomach locations (i.e., from Data_Geostat)
 load(here("new_test", 
           "spiny-dogfish_season-spring_covar-NA_lognm-pl-independent-years-no2spatial", 
           "model-settings.RData"))
 
+datgeo <- Data_Geostat %>%
+  rename(Season = season) %>%
+  filter(!is.na(Catch_KG)) %>%
+  mutate(Season = tolower(Season)) %>%
+  mutate(Herring = ifelse(Catch_KG > 0, "present", "absent")) %>%
+  st_as_sf(coords = c("Lon", "Lat"), crs = 4326)
+
+datknots <- as_tibble(Spatial_List$loc_x) %>%
+  mutate(
+    E_m = E_km*1000,
+    N_m = N_km*1000,
+  ) %>%
+  st_as_sf(coords = c("E_m", "N_m"), crs = 32619) %>%
+  st_transform(crs = 4326)
+
+
 # Make a map
-ggplot(all_trawls, aes(x = declon, y = declat)) +
-  geom_point(color = "grey", alpha = 0.1, pch = 20) +
+p <- ggplot() +
   borders("world", fill = "grey", colour = "white") +
   borders("state", fill = "grey", colour = "white") +
-  coord_quickmap(xlim = c(-77, -65), ylim = c(35, 46)) + 
+  geom_sf(data = filter(all_trawls, Year >= min(datgeo$Year) & Season == "spring"), 
+          mapping = aes(), 
+          color = "grey", 
+          alpha = 0.1, 
+          pch = 20,
+          size = 1) +
+  geom_sf(data = datknots, 
+          color = "black", 
+          pch = 3, 
+          size = 1) +
+  geom_sf(data = datgeo, 
+          aes(color = Herring, fill = Herring), 
+          alpha = 0.7, 
+          size = 1,
+          show.legend = "point") +
+  scale_color_manual(values = c("absent" = "grey40", "present" = "mediumblue")) +
+  scale_fill_manual(values = c("absent" = "grey40", "present" = "mediumblue")) +
+  coord_sf(xlim = c(-77, -65), ylim = c(35, 46)) + 
+  facet_wrap(~Year, ncol = 9) +
   map_theme()
+ggsave(paste0(config_file_name, ".pdf"), 
+       plot = p,
+       path = here::here("output", "map-tows-stomachs-and-knots"),
+       width = 13.2, height = 10.2, units = "in")
 
-
-
-
-scale_color_viridis_c(
-  option = "inferno", 
-  name = "log(Consumption)") +
-  
-
-
-# Check the plot of the knot locations and the data
-plot(Extrapolation_List$Data_Extrap$E_km, Extrapolation_List$Data_Extrap$N_km, 
-     pch = ".", xlab = "Easting (km)", ylab = "Northing (km)")
-points(Spatial_List$loc_i[,1], Spatial_List$loc_i[,2],
-       col = "blue", pch = 20)
-points(Spatial_List$loc_x[,1], Spatial_List$loc_x[,2], 
-       col = "yellow", pch = 19)
-
-# Check variability in the second componenet of the model
-Data_Geostat %>%
-  mutate(prey_pres = ifelse(Catch_KG > 0, "present", "absent")) %>%
-  # filter(Catch_KG > 0) %>%
-  # ggplot(aes(Lon, Lat, color = Catch_KG)) +
-  ggplot(aes(Lon, Lat, color = prey_pres)) +
-  geom_point() +
-  facet_wrap(~Year)
+# Check a subset of years
+ggplot() +
+  borders("world", fill = "grey", colour = "white") +
+  borders("state", fill = "grey", colour = "white") +
+  geom_sf(data = filter(all_trawls, Year %in% c(1990:1995)),
+          mapping = aes(), color = "grey", alpha = 0.1, pch = 20
+  ) +
+  geom_sf(data = datknots, color = "black", pch = 4, size = 1.5) +
+  geom_sf(data = filter(datgeo, Year %in% c(1990:1995)),
+          aes(color = Herring, fill = Herring), 
+          alpha = 0.7, show.legend = "point"
+  ) +
+  scale_color_manual(values = c("absent" = "grey40", "present" = "mediumblue")) +
+  scale_fill_manual(values = c("absent" = "grey40", "present" = "mediumblue")) +
+  coord_sf(xlim = c(-77, -65), ylim = c(35, 46)) + 
+  facet_wrap(~Year) +
+  map_theme()
