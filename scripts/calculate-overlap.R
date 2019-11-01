6dd5947e18a04d7c7a70e8e651d58636d4f7ff8a
Elizabeth Ng <elng@uw.edu>
2019-10-31 18:29:51 -0700

    Update calculate-overlap.R

--- scripts/calculate-overlap.R
+++ scripts/calculate-overlap.R
@@ -2,9 +2,21 @@
 
 library(readr)
 library(here)
+library(tidyverse)
 
 # Locate the formatted trawl data
 dat <- read_rds(here("output", "data_formatted", "dat_trawl.rds"))
 
-# Species names are different, lat lon are different
-# Change those on data formatting side? What script did I use?
+# Species need to use the same mesh for overlap calculations. 
+# How many trawl locations are there per predator species?
+# In this data set they should all have the same number. 
+
+dat %>% 
+  mutate(pres = ifelse(catch_kg > 0, 1, 0)) %>%
+  group_by(species, year, season) %>%
+  summarize(n = n(),
+            n_pres = sum(pres, na.rm = TRUE),
+            tot_kg = sum(catch_kg, na.rm = TRUE)
+            ) %>%
+  mutate(p_pres = 100*(round(n_pres/n, 2))) %>% View()
+  
