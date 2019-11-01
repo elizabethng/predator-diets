ff59216b3f6673ce4c050de007de4c851e1ad17b
Elizabeth Ng <elng@uw.edu>
2019-10-31 17:48:47 -0700

    Update to have similar colnames and structure as diet data

--- scripts/02_process_trawl_data.R
+++ scripts/02_process_trawl_data.R
@@ -3,19 +3,20 @@
 library(tidyverse)
 library(here)
 
-save_data = TRUE
+save_data <- TRUE
 
 
 # Clean Data --------------------------------------------------------------
 # Remove spatial outliers
 # Add tow ID
 
-rawdata = read_csv(here("data", "Ng_OPS.txt"), guess_max = 365080)
+rawdata <- read_csv(here("data", "Ng_OPS.txt"), guess_max = 365080)
 
-mydata = rawdata %>% 
+mydata <- rawdata %>% 
   filter(!(LAT > 37.5 & LON < -75.6)) %>%
   filter(!(LAT < 34.5 & LON > -75.5)) %>%
-  mutate(towid = paste(CRUISE6, STATION, SV_VESSEL)) %>%
+  mutate(towid = paste(CRUISE6, as.numeric(STATION)))%>%
+  mutate(towid_unique = paste(CRUISE6, STATION, SV_VESSEL)) %>%
   rename(
     ATLANTIC_HERRING_wt = CLUPEA_HARENGUS_WT,
     ATLANTIC_COD_wt = GADUS_MORHUA_WT,
@@ -27,27 +28,45 @@ mydata = rawdata %>%
 
 # Check duplicated tow ids (add in SV_VESSEL to towid removes all duplicates except for one)
 # Edit towid for lone duplicated tow
-towid_dupes = mydata$towid[duplicated(mydata$towid)]
-tow_dupes = filter(mydata, towid %in% towid_dupes)
+towid_dupes <- mydata$towid_unique[duplicated(mydata$towid_unique)]
+tow_dupes <- filter(mydata, towid_unique %in% towid_dupes)
 
-mydata[mydata$towid == towid_dupes,][2,"towid"] = paste(towid_dupes, "a")
+mydata[mydata$towid_unique == towid_dupes,][2,"towid_unique"] <- paste(towid_dupes, "a")
 
 
-# Remove columns ----------------------------------------------------------
+# Tidy data ---------------------------------------------------------------
+# Fix seasons and remove extraneous columns
 
-dat = mydata %>%
+
+fixdat <- mydata %>%
+  pivot_longer(
+    cols = contains("_wt"),
+    names_to = "species",
+    values_to = "catch_kg"
+  ) %>%
+  mutate(
+    species = str_remove(species, "_wt"),
+    species = str_replace(species, "_", " ")
+  ) %>%
+  mutate(
+    season = ifelse(SEASON == "FALL"|SEASON == "SUMMER", "FALL", "SPRING")
+  ) %>%
   select(
     towid,
-    SV_VESSEL,
+    towid_unique,
     YEAR,
-    SEASON,
+    SV_VESSEL,
+    season,
     LAT,
     LON,
-    ATLANTIC_HERRING_wt,
-    ATLANTIC_COD_wt,
-    SILVER_HAKE_wt,
-    SPINY_DOGFISH_wt,
-    GOOSEFISH_wt
+    species,
+    catch_kg
+  ) %>%
+  rename(
+    vessel = SV_VESSEL,
+    year = YEAR,
+    declon = LON,
+    declat = LAT
   )
 
 
@@ -61,30 +80,20 @@ dat = mydata %>%
 # change NAs to 0s
 # change tmp to NAs
 
-fixdat = dat %>%
+dat <- fixdat %>%
   mutate(
-    ATLANTIC_HERRING_wt = ifelse(ATLANTIC_HERRING_wt == 0, 9999999999, ATLANTIC_HERRING_wt),
-    ATLANTIC_COD_wt = ifelse(ATLANTIC_COD_wt == 0, 9999999999, ATLANTIC_COD_wt),
-    SILVER_HAKE_wt = ifelse(SILVER_HAKE_wt == 0, 9999999999, SILVER_HAKE_wt),
-    SPINY_DOGFISH_wt = ifelse(SPINY_DOGFISH_wt == 0, 9999999999, SPINY_DOGFISH_wt),
-    GOOSEFISH_wt = ifelse(GOOSEFISH_wt == 0, 9999999999, GOOSEFISH_wt)) %>%
-  replace_na(list(
-    ATLANTIC_HERRING_wt = 0,
-    ATLANTIC_COD_wt = 0,
-    SILVER_HAKE_wt = 0,
-    SPINY_DOGFISH_wt = 0,
-    GOOSEFISH_wt = 0)) %>%
-  mutate(
-    ATLANTIC_HERRING_wt = ifelse(ATLANTIC_HERRING_wt == 9999999999, NA, ATLANTIC_HERRING_wt),
-    ATLANTIC_COD_wt = ifelse(ATLANTIC_COD_wt == 9999999999, NA, ATLANTIC_COD_wt),
-    SILVER_HAKE_wt = ifelse(SILVER_HAKE_wt == 9999999999, NA, SILVER_HAKE_wt),
-    SPINY_DOGFISH_wt = ifelse(SPINY_DOGFISH_wt == 9999999999, NA, SPINY_DOGFISH_wt),
-    GOOSEFISH_wt = ifelse(GOOSEFISH_wt == 9999999999, NA, GOOSEFISH_wt))
+    catch_kg = ifelse(catch_kg == 0, 9999999999, catch_kg),
+    catch_kg = replace_na(catch_kg, 0),
+    catch_kg = ifelse(catch_kg == 9999999999, NA, catch_kg)
+  )
+
+# 0  := none of that species were caught
+# NA := no weights were recorded (but species was present) 
 
 
 
 # Save output -------------------------------------------------------------
 if(save_data == TRUE){
-  saveRDS(fixdat, file = here("output", "data_formatted", "dat_trawl.rds"))
+  saveRDS(dat, file = here("output", "data_formatted", "dat_trawl.rds"))
 }
 
