4ba52534bd2c0c708c83e08c45e60fdf8fa4b18e
Elizabeth Ng <elng@uw.edu>
2019-11-08 14:18:30 -0800

    Load, format, combine diet index and overlap index

--- scripts/24_plot-overlap-and-diet.R
+++ scripts/24_plot-overlap-and-diet.R
@@ -2,12 +2,26 @@
 
 library(tidyverse)
 
-# Load data
+# Load data and format for combining
 gitdir <- "C:/Users/Elizabeth Ng/Documents/GitHub/predator-diets"
-overlapindex <- readr::read_rds(file.path(gitdir, "output", "overlap_index.rds"))
-dietindex <- readr::read_rds(file.path(gitdir, "output", "diet_index.rds"))
+rawoverlapindex <- readr::read_rds(file.path(gitdir, "output", "overlap_index.rds"))
+rawdietindex <- readr::read_rds(file.path(gitdir, "output", "diet_index.rds"))
 
 
-# Clean up overlap index
-overlapindex <- overlapindex %>%
-  mutate(pred = gsub("_", " "))
+
+# Format for combining
+dietindex <- rawoverlapindex %>%
+  dplyr::filter(is.na(reason)) %>%
+  select(-SD_log, -SD_mt, -reason) %>%
+  mutate(density = scale(density)[,1]) %>%
+  rename(diet_index = density) %>%
+  pivot_longer(cols = diet_index, names_to = "index", values_to = "value")
+
+
+overlapindex <- rawdietindex %>%
+  rename(species = pred, 
+         overlap_index = bhat) %>%
+  mutate(overlap_index = scale(overlap_index)[,1]) %>%
+  pivot_longer(cols = overlap_index, names_to = "index", values_to = "value")
+
+indexdat <- bind_rows(dietindex, overlapindex)
