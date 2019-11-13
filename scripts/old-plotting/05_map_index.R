7d6901a70861b948b7367df97ccc994c4ebeec98
Elizabeth Ng <elng@uw.edu>
2019-11-12 20:21:22 -0800

    Update 05_map_index.R

--- scripts/old-plotting/05_map_index.R
+++ scripts/old-plotting/05_map_index.R
@@ -1,62 +1,32 @@
 # Make my own plots of density that are easier to visualize
 # Should now (7/11/19) be able to skip data processing part and just read_csv(my_map_dat.csv) from the appropriate results folder
 
-library(tidyverse)
-library(here)
-library(gganimate)
+library("tidyverse")
+library("gganimate")
+library("here")
 
+topmods <- readr::read_rds(here::here("output", "top_diet.rds"))
 
-results_files = c("herring_density_plg_rw", 
-                  "cod_density_plg_rw", 
-                  "dogfish_density_plg_rw",
-                  "cod_plg_rw",
-                  "dogfish_plg_rw")[5]
+map_dat <- topmods %>%
+  transmute(knot_density = purrr::map(output, "knot_density")) %>%
+  ungroup() %>%
+  tidyr::unnest(knot_density) %>%
+  mutate(ifelse(is.na(exclude_reason), density, NA)) %>%
+  filter(Include == TRUE) %>%
+  mutate(Year = year)
 
-path_name = paste("output", "VAST", "_runs_for_popdy_meeting", sep = "/")
-
-
-
-# Knot locations
-all_locations = read.csv(here("PlotDF_output.csv")) %>%
-  select(-X)
-
-
-# Estimated densities at knots
-load(here(path_name, results_files, "Save.RData"))
-est_dens = Save$Report$D_xcy 
-
-# make estimated densities easier to manipulate
-# Might want to change this to load from results rather than assuming...
-n_years = dim(est_dens)[3]
-
-tmp = numeric()
-for(i in 1:n_years){
-  tmp = c(tmp, est_dens[,,i])
-}
-
-all_dens = tibble(
-  year = sort(rep((2016-n_years):2015, 100)),
-  x2i = rep(1:100, n_years),
-  density = tmp)
-
-
-# Join each year of data with the knot locations for plotting
-map_dat = left_join(all_dens, all_locations) %>%
-  mutate(density_log = log(density))
-
-# missing 34....
-map_dat[!complete.cases(map_dat),]
-map_dat = map_dat[complete.cases(map_dat),]
-
-# Try mapping animation
-p = ggplot(filter(map_dat, Include == TRUE), 
-       aes(x = Lon, y = Lat, color = density_log)) +
+# Check one plot
+map_dat %>%
+  filter(species == "atlantic cod",
+         season == "spring",
+         year == 2015) %>%
+  ggplot(aes(x = Lon, y = Lat, color = density_log)) +
   geom_point() +
   scale_color_viridis_c(
     option = "inferno", 
     name = "log(Density)") +
   borders("world", fill = "grey", colour = "white") +
-  coord_quickmap(xlim = c(-77, -63), ylim = c(37, 47)) +
+  coord_quickmap(xlim = c(-77, -63), ylim = c(30, 47)) +
   theme(panel.grid.major = element_line(color = "white"),
         panel.background = element_blank(),
         axis.title.x = element_blank(),
@@ -64,43 +34,119 @@ p = ggplot(filter(map_dat, Include == TRUE),
         axis.ticks.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) + 
-  theme(legend.position = "bottom") +
-  labs(title = 'Year: {closest_state}') +
-  transition_states(year,
-                    transition_length = 2,
-                    state_length = 5)
-  # facet_wrap(~year)
+        axis.ticks.y = element_blank()) +
+  facet_wrap(~Year)
 
-anim_save(animation = p, filename = here("animated_dogfish_consumption.gif"))
+map_fun <- function(dat){
+  p <- ggplot(dat, aes(x = Lon, y = Lat, color = density_log)) +
+    geom_point() +
+    scale_color_viridis_c(
+      option = "inferno", 
+      name = "log(Density)",
+      limits = range(map_dat$density_log))
+    borders("world", fill = "grey", colour = "white") +
+    coord_quickmap(xlim = c(-77, -63), ylim = c(30, 47)) +
+    theme(panel.grid.major = element_line(color = "white"),
+          panel.background = element_blank(),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank()) +
+    facet_wrap(~Year)
+  return(p)
+}
+
+map_dat %>%
+  filter(species == "atlantic cod",
+         season == "spring",
+         year == 2015) %>%
+  map_fun()
+
+mymaps <- map_dat %>%
+  group_by(species, season, year) %>% 
+  nest() %>%
+  mutate(plot = purrr::map(data, ~ map_fun(.x))) %>%
+  ungroup() %>%
+  mutate(species = gsub(" ", "-", species))
 
 
-# Map comparison of year, all areas
-my_year = c(1977, 2015)
-p = ggplot(filter(map_dat, year %in% my_year), 
-           aes(x = Lon, y = Lat, color = density_log)) +
-  geom_point() +
-  scale_color_viridis_c(
-    option = "inferno", 
-    name = "log(Consumption)") +
-  borders("world", fill = "grey", colour = "white") +
-  coord_quickmap(xlim = c(-79, -63), ylim = c(32, 47)) +
-  facet_wrap(~year) +
-  theme(panel.grid.major = element_line(color = "white"),
-        panel.background = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) + 
-  theme(legend.position = c(0.3, 0.12),
-        legend.direction = "horizontal") + 
-  theme(strip.background = element_blank()) + 
-  theme(text = element_text(size = 13))
-plot(p)
-ggsave(here("output", "plots", paste0(results_files, "_comparison", ".pdf")),
-       plot = p, width = 12, height = 8)
+for(species_ in unique(mymaps$species)){
+  for(season_ in unique(mymaps$season)){
+    pdf(here("output", "plots", "diet-ts", 
+             paste0(paste0(c(species_, season_), collapse = "-"), ".pdf")))
+    mymaps %>%
+      dplyr::filter(species == species_ & season == season_) %>%
+      select(plot) %>%
+      pwalk(~ print(.x))
+      
+    dev.off()
+  }
+}
 
 
+
+# Probably a way to map this using a custom function
+  # ungroup() %>%
+  # group_by(species, season)
+  
+
+
+
+
+# Try mapping animation
+# p <- ggplot(filter(map_dat, Include == TRUE), 
+#        aes(x = Lon, y = Lat, color = density_log)) +
+#   geom_point() +
+#   scale_color_viridis_c(
+#     option = "inferno", 
+#     name = "log(Density)") +
+#   borders("world", fill = "grey", colour = "white") +
+#   coord_quickmap(xlim = c(-77, -63), ylim = c(37, 47)) +
+#   theme(panel.grid.major = element_line(color = "white"),
+#         panel.background = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.text.x = element_blank(),
+#         axis.ticks.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank()) + 
+#   theme(legend.position = "bottom") +
+#   labs(title = 'Year: {closest_state}') +
+#   transition_states(year,
+#                     transition_length = 2,
+#                     state_length = 5)
+#   # facet_wrap(~year)
+# anim_save(animation = p, filename = here("animated_dogfish_consumption.gif"))
+# 
+# 
+# # Map comparison of year, all areas
+# my_year = c(1977, 2015)
+# p = ggplot(filter(map_dat, year %in% my_year), 
+#            aes(x = Lon, y = Lat, color = density_log)) +
+#   geom_point() +
+#   scale_color_viridis_c(
+#     option = "inferno", 
+#     name = "log(Consumption)") +
+#   borders("world", fill = "grey", colour = "white") +
+#   coord_quickmap(xlim = c(-79, -63), ylim = c(32, 47)) +
+#   facet_wrap(~year) +
+#   theme(panel.grid.major = element_line(color = "white"),
+#         panel.background = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.text.x = element_blank(),
+#         axis.ticks.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank()) + 
+#   theme(legend.position = c(0.3, 0.12),
+#         legend.direction = "horizontal") + 
+#   theme(strip.background = element_blank()) + 
+#   theme(text = element_text(size = 13))
+# plot(p)
+# ggsave(here("output", "plots", paste0(results_files, "_comparison", ".pdf")),
+#        plot = p, width = 12, height = 8)
+# 
+# 
 
