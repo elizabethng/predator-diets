06e8182c9ef3ac8b9dfd6bf9c23155fab7faac07
Elizabeth Ng <elng@uw.edu>
2019-11-14 20:14:46 -0800

    Create 05_plot-length-relationship.R

---
+++ scripts/05_plot-length-relationship.R
@@ -0,0 +1,33 @@
+# Check and plot predator covariates
+# Did not keep size category coefficeints 
+# !! [ ] could change that
+# So just plot lengths for all
+
+library("tidyverse")
+library("here")
+
+
+# Raw data for length information
+# rawdiet <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
+#   dplyr::filter(pdcomnam %in% c("ATLANTIC COD", "SILVER HAKE", "SPINY DOGFISH", "GOOSEFISH"))
+
+topdiet <- readr::read_rds(here("output", "top_diet.rds"))
+
+# Get length covariates
+lencoefs <- topdiet %>%
+  mutate(estimates = purrr::map(output, "estimates")) %>%
+  select(season, species, covars, estimates) %>%
+  unnest(cols = c(estimates)) %>% 
+  # filter(covars != "int, sizecat") %>%
+  filter(covariate )
+  
+
+  
+  
+  
+# Get length data from data
+catcoefs <- topdiet %>%
+  mutate(estimates = purrr::map(output, "estimates")) %>%
+  select(season, species, covars, estimates) %>%
+  unnest(cols = c(estimates)) %>% 
+  filter(covars == "int, sizecat")
