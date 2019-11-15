32004e8a5f7efa26b3032d425e25f87d12d9a911
Elizabeth Ng <elng@uw.edu>
2019-11-15 10:44:29 -0800

    Delete old attempts

--- scripts/05_plot-length-relationship.R
+++ scripts/05_plot-length-relationship.R
@@ -32,11 +32,6 @@ alldat <- left_join(lencoefs, lendat, by = c("season", "species")) %>%
   expand_grid(z_score = seq(-2, 2, length.out = 30)) %>%
   mutate(
     length = sd*z_score + mean,
-    # pred_z = pdlenz*z_score,
-    # pred_z2 = pdlenz2*(z_score^2),
-    # pred_len = (sd*pred_z + mean),
-    # pred_len2 = (sd*pred_z2 + mean),
-    # effect = pred_len + pred_len2,
     effect = pdlenz*z_score + pdlenz2*(z_score^2)
   ) %>%
   mutate(predictor = ifelse(predictor == "pred1", "presence", "amount"))
@@ -45,93 +40,20 @@ alldat <- left_join(lencoefs, lendat, by = c("season", "species")) %>%
 
 ggplot(alldat, aes(x = length, y = effect, group = paste(season, species, predictor), color = season)) +
   geom_line() +
-  facet_grid(predictor ~ species) +
+  facet_grid(predictor ~ species, scales = "free_x") +
   theme_bw()
 ggsave(here("output", "plots", "length-effects.pdf"), width = 6, height = 3.5, units = "in")
 
   
-
-ggplot(alldat) +
-  geom_abline()
-
-# If I do equations, I only need the means and sds
-
-
-
-
-
-# Join in covariates for calculation
-preddat <- lendat %>%
-  select(species, season, year, pdlen, declon, declat) %>%
-  left_join(lencoefs, by = c("season", "species")) %>%
-  mutate(
-    mean_len = mean(pdlen),
-    sd_len = sd(pdlen),
-    z_calc = (pdlen - mean_len)/sd_len,
-    pred_val = pdlenz*z_calc,
-    pred_val2 = pdlenz2*(z_calc^2)
-  ) %>%
-  mutate(
-    len_effect = pred_val*sd_len + mean_len,
-    len_effect2 = pred_val2*sd_len + mean_len
-  ) %>%
-  na.omit() %>%
-  select(species, season, year, pdlen, len_effect, len_effect2, predictor) %>%
-  pivot_longer(cols = c(len_effect, len_effect2), names_to = "length", values_to = "predicted_value")
-
-ggplot(preddat, aes(x = pdlen, y = predicted_value, color = length)) +
-  geom_point() +
-  facet_grid(species~predictor)
-
-ggplot(preddat, aes(x = pdlen, y = predicted_value, color = length)) +
-  geom_point() +
-  facet_grid(species~predictor)
-
-  
-
-preddat %>% 
-  sample_n(100) %>%
-  ggplot(aes(x = z_calc, y = pred_val)) +
-  geom_point()
-
-preddat %>% 
-  sample_n(100) %>%
-  ggplot(aes(x = z_calc, y = pred_val2)) +
-  geom_point()
-
-
-
-preddat %>% 
-  sample_n(100) %>%
-  ggplot(aes(x = pdlen, y = len_effect)) +
-  geom_point()
-
-preddat %>% 
-  sample_n(100) %>%
-  ggplot(aes(x = pdlen, y = len_effect2)) +
-  geom_point()
-
-
-
-
-
-# Quick map of length distribution
-  
-  
-  
-  
-  
-  
-  
-
-
 
 
   
 
 
 # ISSUE--come back to this with more time
-#      --problem was myspecies vs species
+# - problem was myspecies vs species
+# - renaming one will probably fix this and let me use the 
+#   standard syntax (unnest(cols = c(data)))
 topdiet %>%
   select(data) %>%
   unnest(cols = c(data))
