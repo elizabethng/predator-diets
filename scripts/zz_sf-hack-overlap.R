9e2c26f12186ba1445e7b8aa5b674332f619e9ac
Elizabeth Ng <elng@uw.edu>
2019-11-26 15:53:19 -0800

    Finish nonspatial calcs and clean up

--- scripts/zz_sf-hack-overlap.R
+++ scripts/zz_sf-hack-overlap.R
@@ -149,244 +149,38 @@ preddat <- filter(scaledat, species != "atlantic herring") %>%
 # Join prey data to predator data by cell/year/season/species
 
 predpreydat <- inner_join(preydat, preddat, by = c("id", "season", "year"))
-# Should check this, returns 3 million rows...
 
 
-#    d. 
-#    e. Calculate overlap metric
+# 3e. Calculate overlap metric --------------------------------------------
+# Should just be able to go row wise
 
-
-# Join back to spatial
-# agg_dat <- left_join(grid, nonspat, by = "id")
-
-# check dims
-# nrow(nonspat)
-# nrow(agg_dat)
-
-# check for missing years
-# group_by(nonspat, species, season, year) %>%
-#   summarize(n = n()) %>%
-#   pivot_wider(names_from = species, values_from = n) %>%
-#   View("year_check")
-# # Bunch of NAs need to be investigated/dropped
-# agg_dat %>% drop_na() # just one row
-# filter(agg_dat, is.na(species))
-# filter(agg_dat, id == 3) # southern area cell that probably had no obs
-# plot(grid)
-# plot(filter(grid, id != 3))
-
-
-
-
-
-
-jj1 <- filter(preydat, year == 1988, season == "fall")
-jj2 <- filter(preddat, year == 1988, season == "fall", pred == "spiny dogfish")
-
-
-jjoin <- st_join(jj2, jj1, join = st_equals, left = FALSE)
-nrow(jj1)
-nrow(jj2)
-nrow(jjoin)
-
-
-
-matched <- jj1[jj1$id %in% jj2$id,]
-matched0 <- jj2[jj2$id %in% jj1$id,]
-# 132 that actually match based on ID, but why don't the geometries match?
-plot(matched)
-plot(matched0)
-
-matched$id
-st_equals(matched[1,], matched0[1,])
-st_equals(matched, matched0)
-st_equals(jj1, jj2)
-
-
-
-
-bhatdat <- full_join(preddat, preydat, by = c("season", "year", "id")) %>%
-  rename(
-    pred = species.x,
-    pred_dens = density_scaled.x,
-    prey = species.y,
-    prey_dens = density_scaled.y
-  ) %>%
+bhat <- ungroup(predpreydat) %>%
   mutate(bhat = sqrt(pred_dens*prey_dens))
 
 
 
+# 4. Calculate annual index -----------------------------------------------
+# Aggregate over years
+annualindex <- bhat %>%
+  group_by(pred, season, year) %>%
+  summarize(tot_bhat = sum(bhat))
 
-
-
-
-
-
-
-
-
-# Get data into grid form -------------------------------------------------
-# So now I basically need to take data for each species (maybe by season?)
-# and join it to grid to match up the points
-# Then I need to aggregate by polygon ids within years to get the average
-# density for each cell. 
-
-# UTMs have all locs, but some Lat Lon don't
-# Could be for the excluded regions?
-# exclude these for now.
-filter(knotdat, is.na(Lat))
-
-alldat <- knotdat %>%
-  filter(!is.na(Lat)) %>%
-  select(-c(knot, E_km, N_km, Include, density_log, exclude_reason)) %>%
-  st_as_sf(coords = c("Lat", "Lon"), crs = 4326) 
-
-
-
-grid_dat <- st_join(grid, alldat, join = st_contains)
-# write_rds(grid_dat, here("output", "grid_dat.rds"))
-
-agg_dat <- grid_dat %>%
-  group_by(id, myid) %>%
-  summarize(mean_density = mean(density))
-
-
-
-# could also try this without the geometry, but I think it's just a big object
-
-
-
-
-
-
-
-
-
-
-test <- st_intersection(prey, grid)
-test2 <- st_join(grid, prey, join = st_intersects)
-
-
-
-
-pred <- filter(smalldat, species != "atlantic herring")
-
-
-myknn <- class::knn(train = prey[,c("E_km", "N_km")],
-                    test = pred[,c("E_km", "N_km")],
-                    cl = prey$knot)
-
-
-
-
-
-
-set.seed(2)
-points <- select(smalldat, E_km, N_km) %>%
-  stats::kmeans(centers = 100)
-
-# Check correspondence
-plot(points$centers)
-sample_n(smalldat, 50) %>% 
-  select(E_km, N_km) %>%
-  points(col = "red")
-
-# Add in new knots
-smalldat$cluster <- points$cluster
-
-# Bhattacharyya's Coefficient
-# sum_{i=1}^{n} sqrt{p_pred_i*p_prey_i}, 
-# where p_pred_i and p_pred_i are 
-# the proportion of the total number of predator and prey
-# in a given area A
-
-smallerdat <- smalldat %>%
-  select(species, season, year, Lat, Lon, density, cluster) %>%
-  # group_by(species, season, year, cluster) %>%
-  mutate(
-    new_density = density, # mean(density),
-    density_scaled = density # new_density/sum(new_density)
-  ) %>%
-  select(-density, -new_density)
-
-
-prey <- filter(smallerdat, species == "atlantic herring") %>%
-  select(-Lat, -Lon) %>%
-  distinct()
-pred <- filter(smallerdat, species != "atlantic herring")%>%
-  select(-Lat, -Lon) %>%
-  distinct()
-
-bhatdat <- full_join(pred, prey, by = c("season", "year", "cluster")) %>%
-  rename(
-    pred = species.x,
-    pred_dens = density_scaled.x,
-    prey = species.y,
-    prey_dens = density_scaled.y
-  ) %>%
-  mutate(bhat = sqrt(pred_dens*prey_dens))
-
-# Check missing-ness
-badobs <- dplyr::filter(bhatdat, is.na(bhat))
-bhatsubset <- filter(bhatdat, !is.na(bhat))
-unique(badobs$cluster)
-unique(bhatsubset$cluster)
-
-
-
-# Aggregate the index (non area-weghted)
-overlapindex <- bhatsubset %>%
-  ungroup() %>%
-  group_by(season, pred, year) %>%
-  summarize(
-    bhat = sum(bhat)
-  ) %>%
-  ungroup() %>%
-  mutate(pred = gsub("_", " ", pred)) %>%
-  mutate(name = paste0(pred, ", ", season))
-
-
-# Plot the indices
-ggplot(overlapindex, aes(x = year, y = bhat, group = name, color = season)) +
-  geom_line() +
-  facet_wrap(~pred) + 
-  theme_bw()
-# ggsave(here::here("output", "plots", "overlap-comparison.pdf"),
-#        width = 9, height = 5, units = "in")
-# 
-# write_rds(overlapindex, here::here("output", "index_overlap.rds"))
-
-# Try spatial plot
-test <- smallerdat %>% 
-  filter(species == "spiny dogfish") %>%
-  ungroup() %>%
-  left_join(bhatsubset, by = c("year", "season", "cluster")) %>%
-  mutate(bhat_scaled = scale(bhat)[,1])
-
-test %>% 
-  group_by(year) %>%
-  summarize(tot_bhat = sum(bhat, na.rm = TRUE)) %>%
-  ggplot(aes(year, tot_bhat)) +
-  geom_line()
-
-test %>%
-  rename(predator = pred) %>%
-  sample_frac(0.5) %>%
-  filter(!is.na(predator)) %>%
-  ggplot(aes(x = Lon, y = Lat, color = bhat)) +
+ggplot(annualindex, aes(x = year, y = tot_bhat, color = season)) +
   geom_point() +
-  scale_color_viridis_c(
-    option = "inferno", 
-    name = "overlap metric"
-  ) + 
-  borders("world", fill = "grey", colour = "white") +
-  coord_quickmap(xlim = c(-77, -63), ylim = c(34, 47)) +
-  theme(panel.grid.major = element_line(color = "white"),
-        panel.background = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
-  facet_wrap(predator ~ season)
+  geom_line() +
+  facet_wrap(~pred)
+
+
+
+# 5. Join back to spatial -------------------------------------------------
+
+bhat_spat <- left_join(grid, bhat, by = "id")
+
+bhat_spat %>%
+  filter(
+    pred == "atlantic cod",
+    year == 1978
+    ) %>%
+  ggplot() +
+  geom_sf(aes(fill = bhat)) +
+  facet_wrap(~season)
