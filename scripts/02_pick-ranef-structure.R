7d02b7f41ade81258bd24e6a95912f5eb2197d69
Elizabeth Ng <elng@uw.edu>
2019-12-09 15:04:06 -0800

    Add code to run for trawl mods
    But not tested since I didn't run the first step yet

--- scripts/02_pick-ranef-structure.R
+++ scripts/02_pick-ranef-structure.R
@@ -94,5 +94,83 @@ write_rds(topmod_data, here("output", "top_st_diet.rds"))
 
 # Trawl data --------------------------------------------------------------
 
-# Trawl mods: trawlrun <- read_rds(here("output", "st_sel_trawl.rds"))
+trawlrun <- read_rds(here("output", "st_sel_trawl.rds"))
+
+allruns <- trawlrun %>% 
+  dplyr::mutate(
+    errors = purrr::map(output,"error"), 
+    worked = purrr::map_lgl(errors, is.null)
+  ) %>% 
+  dplyr::filter(worked) %>% 
+  dplyr::mutate(
+    output = purrr::map(output, "result"),
+    model = basename(config_file_loc), 
+    model = gsub(".R", "", model),
+    covars = purrr::map_chr(covar_columns, ~ gsub(" ", ", ", .x))
+  ) %>%
+  dplyr::select(
+    -contains("_"),
+    -c(errors, worked)) %>%
+  dplyr::mutate(
+    converged = purrr::map_chr(output, "converged"), # Some errors were passed (non numerical argument)
+    converged = ifelse(converged %in% c("TRUE", "FALSE"), converged, NA) 
+  ) %>%
+  dplyr::mutate(
+    aic = purrr::map(output, "aic"),
+    aic = na_if(aic, "NULL")
+  ) %>%
+  unnest(cols = c("aic")) 
+
+# Error messages get caught before going to output$error
+failed <- allruns %>%
+  filter(is.na(converged) | converged == FALSE)
+
+# Check magnitude of effects
+worked <- allruns %>%
+  filter(converged == TRUE) %>%
+  mutate(hatval = purrr::map(output, "estimates")) %>%
+  select(-output, -data)
+
+
+# Create AIC table with a check that variances are ok
+aictabs <- worked %>%
+  dplyr::group_by(species, season) %>%
+  dplyr::arrange(aic) %>%
+  dplyr::mutate(delta_aic = round(aic - min(aic), 1))
+
+
+# check if any hatvals are less than 0.001
+modchecks <- aictabs %>%
+  filter(delta_aic < 2) %>%
+  unnest(cols = c(hatval)) %>%
+  ungroup() %>%
+  filter(
+    covariate %in% c("epsilon", "omega"),
+    estimate != 1  # default value = 1 when no ranef is estimated
+  ) %>%
+  mutate(
+    estimate = abs(estimate),
+    ranef_ok = estimate > 0.001) %>%
+  group_by(species, season, model) %>%
+  mutate(ranef_ok = all(ranef_ok)) %>%
+  nest(hatval = c(covariate, predictor, estimate))
+
+# Write output for next phase of model selection
+topmods <- modchecks %>%
+  filter(ranef_ok == TRUE) %>%
+  mutate(nparm = map_int(hatval, nrow)) %>%
+  ungroup() %>%
+  group_by(species, season) %>%
+  top_n(-1, wt = nparm) %>%
+  ungroup()
+
+# Get data, config file etc.
+topmod_data <- trawlrun %>%
+  dplyr::mutate(
+    model = basename(config_file_loc), 
+    model = gsub(".R", "", model)
+  ) %>%
+  semi_join(topmods, by = c("species", "season", "model"))
+write_rds(topmod_data, here("output", "top_st_trawl.rds"))
+
   
