1619e095fc1f65b692ecb0597ce8f05efc28ac88
Elizabeth Ng <elng@uw.edu>
2019-12-09 15:14:29 -0800

    Update to use new data output from step 2

--- scripts/03_fit-covariates.R
+++ scripts/03_fit-covariates.R
@@ -23,36 +23,29 @@ diagnostic_folder <- file.path("D:", "Dropbox", "Predator_Diets", "output", "VAS
 
 # Diet Data ---------------------------------------------------------------
 
+
+# dietsetup <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
+#   # dplyr::filter(year %in% 1990:1995) %>%
+#   # dplyr::filter(pdcomnam == "SPINY DOGFISH", myseason == "SPRING") %>%
+#   dplyr::filter(pdcomnam %in% c("ATLANTIC COD", "SILVER HAKE", "SPINY DOGFISH", "GOOSEFISH")) %>%
+#   group_by(pdcomnam, myseason) %>%
+#   nest() %>%
+#   mutate(processed_data = purrr::map(data, process_diet_data))
+
+
 # Load top models (don't need to repeat data processing step)
-top_st_mods <- read_rds(here("output", "top_st_diet.rds"))
+dietsetup <- read_rds(here("output", "top_st_diet.rds")) %>%
+  rename(pdcomnam = species, myseason = season) %>%
+  select(pdcomnam, myseason, data, processed_data, config_file_loc)
 
-
-# Filter and process data
-dietsetup <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
-  # dplyr::filter(year %in% 1990:1995) %>%
-  # dplyr::filter(pdcomnam == "SPINY DOGFISH", myseason == "SPRING") %>%
-  dplyr::filter(pdcomnam %in% c("ATLANTIC COD", "SILVER HAKE", "SPINY DOGFISH", "GOOSEFISH")) %>%
-  group_by(pdcomnam, myseason) %>%
-  nest() %>%
-  mutate(processed_data = purrr::map(data, process_diet_data))
-
-# Add model options (covariates, config_files)
 covar_columns <- c(NA,
                    "int sizecat",
                    "int pdlenz",
                    "int pdlenz pdlenz2")
-config_file_loc <- map2_chr("configuration-files", 
-                            c("gamma_ind-yrs_st-full.R", 
-                              "gamma_ind-yrs_st-pres.R", 
-                              "gamma_ind-yrs_st-none.R", 
-                              "lognm_ind-yrs_st-full.R", 
-                              "lognm_ind-yrs_st-pres.R", 
-                              "lognm_ind-yrs_st-none.R"), 
-                            here)
 
-dietrun <- tidyr::expand_grid(dietsetup, covar_columns, config_file_loc)
+dietrun <- tidyr::expand_grid(dietsetup, covar_columns)
 
-# Make file run name and save file location
+# Make file run name and output file location
 dietrun <- dietrun %>%
   dplyr::mutate(run_name = purrr::pmap_chr(
     list("diet",
@@ -72,10 +65,11 @@ dietrun <- dietrun %>%
        strata_file_loc = here("configuration-files", "strata_limits_subset.R"), 
        processed_data, 
        output_file_loc,
-       check_identifiable = TRUE),
+       check_identifiable = FALSE,
+       use_REML = FALSE),
   safe_run_mod))
 
-readr::write_rds(dietrun, path = here("output", "raw_diet.rds"))
+readr::write_rds(dietrun, path = here("output", "cov_sel_diet.rds")) # was raw_diet.rds
 
 
 
