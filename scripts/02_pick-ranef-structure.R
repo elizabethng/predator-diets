e178a6175c53372b572cd21c61dc859668261813
Elizabeth Ng <elng@uw.edu>
2019-12-09 15:02:38 -0800

    Clean up script

--- scripts/02_pick-ranef-structure.R
+++ scripts/02_pick-ranef-structure.R
@@ -1,18 +1,18 @@
 # Script to check aic and random effects magnitude for 
-# random effect structure model selection. 
+# random effect structure model selection fit with REML. 
 # Not yet implemented for trawl output. 
 
-# [ ] Need to pass results to next script where I do model selction for 
-#     the covaraites, and ultimately, re-fit the top model. --> write to a data frame and use for fit.
+# Save results for use by next script to do model 
+# selction for covaraites.
 
 library("tidyverse")
 library("here")
 
-# Trawl mods: trawlrun <- read_rds(here("output", "st_sel_trawl.rds"))
+
+# Diet Models ---------------------------------------------------------------
 
 dietrun <- read_rds(here("output", "st_sel_diet.rds"))
 
-# REML models
 allruns <- dietrun %>% 
   dplyr::mutate(
     errors = purrr::map(output,"error"), 
@@ -38,9 +38,9 @@ allruns <- dietrun %>%
     ) %>%
   unnest(cols = c("aic")) 
 
+# Error messages get caught before going to output$error
 failed <- allruns %>%
-  filter(is.na(converged) | converged == FALSE) # error messages get caught before going to output$erro
-select(failed, -data, -output) # weird that these were all the full st, especially for a few that I thought would fit
+  filter(is.na(converged) | converged == FALSE)
 
 # Check magnitude of effects
 worked <- allruns %>%
@@ -49,29 +49,12 @@ worked <- allruns %>%
   select(-output, -data)
 
 
-covcheck <- worked %>%
-  unnest(cols = c(hatval)) %>%
-  ungroup() %>%
-  filter(
-    covariate %in% c("epsilon", "omega"),
-    estimate != 1  # default value = 1 when no ranef is estimated
-    ) %>%
-  mutate(
-    estimate = abs(estimate),
-    ranef_check = estimate > 0.001)
-
-filter(covcheck, !ranef_check)
-
-# Create AIC tab with a check that variances are ok
-# [sample code]
+# Create AIC table with a check that variances are ok
 aictabs <- worked %>%
   dplyr::group_by(species, season) %>%
   dplyr::arrange(aic) %>%
   dplyr::mutate(delta_aic = round(aic - min(aic), 1))
 
-# Top models are st in presence only, except for
-# - spiny dogfish both seasons support for st in both components (also has the most data)
-# - goosefish spring, spatial in presence only, no temporal (1.9 delAIC, so choose simpler)
 
 # check if any hatvals are less than 0.001
 modchecks <- aictabs %>%
@@ -89,15 +72,6 @@ modchecks <- aictabs %>%
   mutate(ranef_ok = all(ranef_ok)) %>%
   nest(hatval = c(covariate, predictor, estimate))
   
-# Next steps:
-# 1. write aic tables to file
-# 2. how to choose "simpler model" in non hard-coded way?
-# 3. write the species/season/model config file names to use in covariate selection process
-
-# Actually, I may not need to write the aic tables to file,
-# since I probably won't present them in the manuscript. 
-# Instead I can just discuss the top models from maybe one print out. 
-
 # Write output for next phase of model selection
 topmods <- modchecks %>%
   filter(ranef_ok == TRUE) %>%
@@ -114,6 +88,11 @@ topmod_data <- dietrun %>%
     model = gsub(".R", "", model)
   ) %>%
   semi_join(topmods, by = c("species", "season", "model"))
-
 write_rds(topmod_data, here("output", "top_st_diet.rds"))
+
+
+
+# Trawl data --------------------------------------------------------------
+
+# Trawl mods: trawlrun <- read_rds(here("output", "st_sel_trawl.rds"))
   
