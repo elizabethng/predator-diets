675fa3f70072108e8c76b3a01c4ec402e8d1ef9a
Elizabeth Ng <elng@uw.edu>
2019-12-09 15:42:09 -0800

    Create new scripts for processing workflow

---
+++ scripts/04_pick-covariates.R
@@ -0,0 +1,3 @@
+# Process and select top models for diet data to get
+# final covariate structure to use. Save output to use in next
+# step of the analysis.
---
+++ scripts/05_fit-final-models.R
@@ -0,0 +1,13 @@
+# Final step of model selection process, refit the top models
+# for both diet data and trawl data using
+#  * REML
+#  * Doing bias correction
+#  * Doing fine.scale = TRUE (??)
+#  * Getting output
+#    - Index and SEs
+#    - Parameter estimates
+#    - vcov for fixed effects
+#    - knot-level means
+#    - knot-level SEs
+#    - mesh
+# [ ] What else?
