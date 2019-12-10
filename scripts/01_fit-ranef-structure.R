63b1c6c58f217124ee0a677c23aac7cdba681fab
Elizabeth Ng <elng@uw.edu>
2019-12-10 11:33:26 -0800

    Update naming and make test case easier to run

--- scripts/01_fit-ranef-structure.R
+++ scripts/01_fit-ranef-structure.R
@@ -9,6 +9,8 @@ library("TMB")
 
 
 # Setup -------------------------------------------------------------------
+test <- TRUE
+
 # Load helper functions
 functions <- list.files(here("functions"), full.names = TRUE)
 invisible(sapply(functions, source))
@@ -22,15 +24,21 @@ diagnostic_folder <- file.path("D:", "Dropbox", "Predator_Diets", "output", "VAS
 
 # Diet Data ---------------------------------------------------------------
 # Filter and process data
-dietsetup <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
-  # dplyr::filter(year %in% 1990:1995) %>%
-  # dplyr::filter(pdcomnam == "SPINY DOGFISH", myseason == "SPRING") %>%
-  dplyr::select(-season) %>%
-  dplyr::filter(pdcomnam %in% c("ATLANTIC COD", "SILVER HAKE", "SPINY DOGFISH", "GOOSEFISH")) %>%
-  rename(species = pdcomnam, season = myseason) %>% # should push this change all the way to processign stage
-  group_by(species, season) %>%
-  nest() %>%
-  mutate(processed_data = purrr::map(data, process_diet_data))
+if(test == TRUE){
+  dietsetup <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
+    dplyr::filter(year %in% 1990:1995) %>%
+    dplyr::filter(predator == "spiny dogfish", season == "spring") %>%
+        group_by(predator, season) %>%
+    nest() %>%
+    mutate(processed_data = purrr::map(data, process_diet_data))
+}else if(test == FALSE){
+  dietsetup <- readr::read_rds(here("data", "processed", "dat_preds_all.rds")) %>%
+    dplyr::filter(predator %in% c("atlantic cod", "silver hake", "spiny dogfish", "goosefish")) %>%
+    group_by(predator, season) %>%
+    nest() %>%
+    mutate(processed_data = purrr::map(data, process_diet_data))
+}
+
 
 # Add model options (covariates, config_files)
 covar_columns <- c("int sizecat pdlenz pdlenz2")
@@ -47,7 +55,7 @@ dietrun <- tidyr::expand_grid(dietsetup, covar_columns, config_file_loc)
 dietrun <- dietrun %>%
   dplyr::mutate(run_name = purrr::pmap_chr(
     list("diet",
-         species,
+         predator,
          season, 
          covar_columns, 
          config_file_loc),
@@ -67,39 +75,46 @@ dietrun <- dietrun %>%
        use_REML = TRUE),
   safe_run_mod))
 
-readr::write_rds(dietrun, path = here("output", "st_sel_diet.rds"))
-
+readr::write_rds(dietrun, path = here("output", "sel_st_diet.rds"))
+rm(c(dietrun, dietsetup))
 
 
 
 # Trawl Data --------------------------------------------------------------
 # Filter and process data
-trawlsetup <- readr::read_rds(here("data", "processed", "dat_trawl.rds")) %>%
-  dplyr::filter(year %in% 1990:1995) %>%
-  filter(pdcomnam == "SPINY DOGFISH", myseason == "SPRING") %>%
-  group_by(pdcomnam, myseason) %>%
-  nest() %>%
-  mutate(processed_data = purrr::map(data, process_trawl_data))
+
+if(test == TRUE){
+  trawlsetup <- readr::read_rds(here("data", "processed", "dat_trawl.rds")) %>%
+    dplyr::filter(year %in% 1990:1995) %>%
+    filter(predator == "spiny dogfish", season == "spring") %>%
+    group_by(predator, season) %>%
+    nest() %>%
+    mutate(processed_data = purrr::map(data, process_trawl_data))
+}else if(test == FALSE){
+  trawlsetup <- readr::read_rds(here("data", "processed", "dat_trawl.rds")) %>%
+    group_by(predator, season) %>%
+    nest() %>%
+    mutate(processed_data = purrr::map(data, process_trawl_data))
+}
+
 
 # Add in model options (covariates, config_files)
 covar_columns <- NA
 config_file_loc <- map2_chr("configuration-files", 
                             c("gamma_ind-yrs_st-full.R", 
                               "gamma_ind-yrs_st-pres.R", 
-                              "gamma_ind-yrs_st-none.R", 
-                              "lognm_ind-yrs_st-full.R", 
-                              "lognm_ind-yrs_st-pres.R", 
-                              "lognm_ind-yrs_st-none.R"), 
+                              "gamma_ind-yrs_s0-pres.R",
+                              "gamma_ind-yrs_st-none.R"),
                             here)
 
-trawlrun <- tidyr::expand_grid(trawlsetup, covar_columns, config_file_loc)[1, ]
+trawlrun <- tidyr::expand_grid(trawlsetup, covar_columns, config_file_loc)
 
 # Make file run name and save file location
 trawlrun <- trawlrun %>%
   dplyr::mutate(run_name = purrr::pmap_chr(
     list("trawl", 
-         pdcomnam, 
-         myseason, 
+         predator, 
+         season, 
          covar_columns, 
          config_file_loc),
     make_run_name
@@ -118,6 +133,4 @@ trawlrun <- trawlrun %>%
          check_identifiable = FALSE,
          use_REML = TRUE),
     safe_run_mod))
-
-
-readr::write_rds(trawlrun, path = here::here("output", "st_sel_trawl.rds")) # previously raw_trawl.rds
+readr::write_rds(trawlrun, path = here::here("output", "sel_st_trawl.rds"))
