0ee12429a2fd68016caf5cd4b2b23fc2a4a7009f
Elizabeth Ng <elng@uw.edu>
2019-12-09 17:13:59 -0800

    Add code to run final round of models
    But need to update run_mod function to get all output

--- scripts/05_fit-final-models.R
+++ scripts/05_fit-final-models.R
@@ -11,3 +11,52 @@
 #    - knot-level SEs
 #    - mesh
 # [ ] What else?
+
+
+library("tidyverse")
+library("here")
+library("VAST")
+library("TMB")
+
+
+
+# Setup -------------------------------------------------------------------
+# Load helper functions
+functions <- list.files(here("functions"), full.names = TRUE)
+invisible(sapply(functions, source))
+
+Version <- FishStatsUtils::get_latest_version()
+safe_run_mod <- purrr::safely(run_mod_fast)
+
+# Set VAST output location
+diagnostic_folder <- file.path("D:", "Dropbox", "Predator_Diets", "output", "VAST")
+
+
+# Diet Data ---------------------------------------------------------------
+
+# Load top models
+dietrun <- read_rds(here("output", "top_cov_diet.rds")) %>%
+  rename(pdcomnam = species, myseason = season) %>%
+  select(pdcomnam, 
+         myseason, 
+         data, 
+         processed_data, 
+         config_file_loc, 
+         covar_columns, 
+         run_name,
+         output_file_loc) # may want to modify in future?
+
+# Run the model
+dietrun <- dietrun %>%
+  dplyr::mutate(output = purrr::pmap(
+    list(covar_columns, 
+         config_file_loc, 
+         strata_file_loc = here("configuration-files", "strata_limits_subset.R"), 
+         processed_data, 
+         output_file_loc,
+         check_identifiable = FALSE,
+         use_REML = TRUE),
+    safe_run_mod))
+
+readr::write_rds(dietrun, path = here("output", "top_final_diet.rds"))
+
