a0b903916164c8d5b34bd324db3546f241aa463d
Elizabeth Ng <elng@uw.edu>
2019-12-10 12:09:09 -0800

    Update diet data workflow (names, more robust selection)

--- scripts/02_pick-ranef-structure.R
+++ scripts/02_pick-ranef-structure.R
@@ -11,7 +11,7 @@ library("here")
 
 # Diet Models ---------------------------------------------------------------
 
-dietrun <- read_rds(here("output", "st_sel_diet.rds"))
+dietrun <- read_rds(here("output", "select_st_diet.rds"))
 
 allruns <- dietrun %>% 
   dplyr::mutate(
@@ -42,43 +42,42 @@ allruns <- dietrun %>%
 failed <- allruns %>%
   filter(is.na(converged) | converged == FALSE)
 
-# Check magnitude of effects
+# Process models without errors
 worked <- allruns %>%
   filter(converged == TRUE) %>%
   mutate(hatval = purrr::map(output, "estimates")) %>%
-  select(-output, -data)
+  select(-output, -data) %>%
+  dplyr::group_by(predator, season) %>%
+  dplyr::mutate(delta_aic = aic - min(aic))
 
 
-# Create AIC table with a check that variances are ok
-aictabs <- worked %>%
-  dplyr::group_by(species, season) %>%
-  dplyr::arrange(aic) %>%
-  dplyr::mutate(delta_aic = round(aic - min(aic), 1))
-
-
-# check if any hatvals are less than 0.001
-modchecks <- aictabs %>%
-  filter(delta_aic < 2) %>%
+# check if any hatvals for top models are small
+modchecks <- worked %>%
   unnest(cols = c(hatval)) %>%
   ungroup() %>%
-  filter(
-    covariate %in% c("epsilon", "omega"),
-    estimate != 1  # default value = 1 when no ranef is estimated
-  ) %>%
   mutate(
-    estimate = abs(estimate),
-    ranef_ok = estimate > 0.001) %>%
-  group_by(species, season, model) %>%
-  mutate(ranef_ok = all(ranef_ok)) %>%
-  nest(hatval = c(covariate, predictor, estimate))
+    ranef = ifelse(test = covariate %in% c("epsilon", "omega"),
+                   yes = TRUE,
+                   no = FALSE),
+    ranef_ok = ifelse(test = ranef == TRUE,
+                      yes = ifelse(test = estimate == 1, # default value = 1 (no ranef is estimated)
+                                   yes = NA,
+                                   no = abs(estimate) > 0.001),
+                      no = NA)
+  ) %>%
+  group_by(predator, season, model, covars, aic, delta_aic) %>%
+  summarize(
+    ranef_ok = all(ranef_ok, na.rm = TRUE),
+    ranef_n = sum(ranef)
+  ) %>%
+  ungroup() 
   
 # Write output for next phase of model selection
 topmods <- modchecks %>%
-  filter(ranef_ok == TRUE) %>%
-  mutate(nparm = map_int(hatval, nrow)) %>%
-  ungroup() %>%
-  group_by(species, season) %>%
-  top_n(-1, wt = nparm) %>%
+  filter(delta_aic < 2,
+         ranef_ok == TRUE) %>%
+  group_by(predator, season) %>%
+  top_n(-1, wt = ranef_n) %>%
   ungroup()
 
 # Get data, config file etc.
@@ -87,14 +86,14 @@ topmod_data <- dietrun %>%
     model = basename(config_file_loc), 
     model = gsub(".R", "", model)
   ) %>%
-  semi_join(topmods, by = c("species", "season", "model"))
+  semi_join(topmods, by = c("predator", "season", "model"))
 write_rds(topmod_data, here("output", "top_st_diet.rds"))
 
 
 
 # Trawl data --------------------------------------------------------------
 # Not tested, need to run new 01_fit-ranef-structure
-trawlrun <- read_rds(here("output", "st_sel_trawl.rds"))
+trawlrun <- read_rds(here("output", "select_st_trawl.rds"))
 
 allruns <- trawlrun %>% 
   dplyr::mutate(
