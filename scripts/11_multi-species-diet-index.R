7c8ade20a6ce7d4f54df14387436c24e4b499a95
Elizabeth Ng <elng@uw.edu>
2020-04-13 13:55:35 -0700

    More attempts at multi-species diet index

--- scripts/11_multi-species-diet-index.R
+++ scripts/11_multi-species-diet-index.R
@@ -33,8 +33,10 @@ moddat <- left_join(dietindex, assessdat, by = "year") %>%
   mutate(year = as.factor(year)) %>%
   drop_na()
 
-ggplot(moddat, aes(stock_index, density, color = predator)) +
-  geom_point()
+ggplot(moddat, aes(stock_index, density, color = season)) +
+  geom_point() + 
+  geom_smooth(method = "lm") +
+  facet_wrap(season ~ predator, nrow = 2, scales = "free_y")
 
 
 # 2. Fit linear model -----------------------------------------------------
@@ -51,12 +53,34 @@ top_mod <- MuMIn::get.models(dd, subset = 1)[[1]]
 summary(top_mod)
 broom::tidy(top_mod)
 
-
+# multipred_model <- lm(stock_index ~ predator + diet_index)
 
 # 3. Extract covariates and make predictions ------------------------------
-
 # Do this by leaving out predator effects? Not entirely clear how to do this. 
 
+predmod <- moddat %>%
+  mutate(predicted = predict(top_mod))
 
+plot(predmod$year, predmod$predicted)
+
+ggplot(predmod, aes(x = year, y = predicted)) +
+  geom_violin()
+
+
+# Try random effect approach ----------------------------------------------
+# Set predator as a random effect? Then integrate out?
+
+# Try normalizing first
+moddat_z <- moddat %>%
+  group_by(season, predator) %>%
+  mutate(density_z = scale(density)[,1],
+         index_z = scale(stock_index)[,1])
+
+ran_mod <- lme4::lmer(stock_index ~ density_z + (density_z | predator), moddat_z)
+summary(ran_mod)
+
+# But I think slopes are random
+ran_mod <- lme4::lmer(stock_index ~ density_z + (1 + density_z | predator), moddat_z)
+summary(ran_mod)
 
 
