b4c4d7af9af8e5afd95b72e0b96a0b19576c3e83
Elizabeth Ng <elng@uw.edu>
2020-04-25 10:52:01 -0700

    Create 00_data-summaries.R

---
+++ scripts/00_data-summaries.R
@@ -0,0 +1,16 @@
+# Create tables summarizing the two types of data used
+
+# For a pub I would like to see a table that summarize the basic data. 
+#   - Number of shots per survey
+#   - How many with positive data
+#   - How many sampled, etc. 
+
+# Use formatted data or raw data?
+
+library("tidyverse")
+library("here")
+
+
+# Raw data ----------------------------------------------------------------
+
+
