0afe03de1388d7e48d44bca1bd691647790c7bea
Elizabeth Ng <elng@uw.edu>
2020-05-01 15:10:42 -0700

    Add correlation plots with my Atlantic herring index
    that is, using the estimated index from spatio-temporal models fit to bottom trawl data. Also add a plot comparing all the different indices. Can really see that the assessment is much smoother. Spiky indices really throw off correlation. Went back to using assessment index.

--- scripts/10_plot-correlations.R
+++ scripts/10_plot-correlations.R
@@ -3,15 +3,76 @@
 # 2. plot one to one comparison of diet index and assessment biomass index
 # 3. plot one to one of overlap and assessment
 
+# Add herring, then add error bars
+# for plotting attemps, can just add arbitraty SE to overlap index?
 
 library("tidyverse")
 library("here")
+
+use_assessment <- TRUE
 
 # Load data and format for combining
 dietindexr <- read_rds(here::here("output", "index_diet.rds"))
 overlapindexr <- read_rds(here::here("output", "index_overlap.rds"))
 assessdatr <- readxl::read_xlsx(here("data", "raw", "TimeSeries.xlsx"))
 
+# add my Atlantic herring index
+herringdatr <- readr::read_rds(here::here("output", "top_final_trawl.rds")) %>%
+  filter(species == "atlantic herring") %>%
+  select(season, species, output) %>%
+  mutate(output = map(output, "result")) %>%
+  mutate(output = map(output, "index")) %>%
+  unnest(output)
+  
+
+
+# Plot indices as time-series ---------------------------------------------
+abundance_indices <- pivot_longer(assessdatr, cols = -Year) %>%
+  filter(name %in% c("SSB (mt)",
+                     "Jan.1 Biomass (mt)",
+                     "Catch (mt)")) %>%
+  mutate(source = "assessment") %>%
+  bind_rows(herringdatr %>%
+              mutate(source = "st index") %>%
+              select(Year, 
+                     source, 
+                     name = season, 
+                     value = Estimate_metric_tons)) %>%
+  bind_rows(dietindexr %>%
+              mutate(name = paste(predator, season),
+                     source = "diet index") %>%
+              select(Year = year,
+                     source,
+                     name,
+                     value = density)) %>%
+  group_by(name) %>%
+  mutate(value_z = scale(value)[,1])
+
+ggplot(abundance_indices, aes(x = Year, y = value_z, group = name, color = source)) +
+  geom_point() +
+  geom_line(size = 1, alpha = 0.5) +
+  scale_color_manual(values = c(
+    scales::muted("blue", l = 50, c = 100), 
+    "grey",
+    scales::muted("red", l = 50, c = 100))) +
+  theme_bw()
+
+mean_diets <- filter(abundance_indices, source == "diet index") %>%
+  group_by(Year, source) %>%
+  summarize(value_z = mean(value_z, na.rm = TRUE)) %>%
+  mutate(name = "average diets") %>%
+  bind_rows(abundance_indices %>%
+              filter(source != "diet index"))
+
+ggplot(mean_diets, aes(x = Year, y = value_z, group = name, color = source)) +
+  geom_point() +
+  geom_line() +
+  scale_color_manual(values = c(
+    scales::muted("blue", l = 50, c = 100), 
+    "grey",
+    scales::muted("red", l = 50, c = 100))) +
+  theme_bw()
+ggsave(here("output", "plots", "index-ts.pdf"), width = 8.5, height = 4)
 
 # 0. Scale and rename -----------------------------------------------------
 diet_index <- dietindexr %>%
@@ -27,15 +88,26 @@ overlap_index <- overlapindexr %>%
   select(-`Overlap index`) %>%
   ungroup()
 
-pred_seas <- select(diet_index, predator, season) %>% distinct()
+if(use_assessment == TRUE){
+  pred_seas <- select(diet_index, predator, season) %>% distinct()
+  
+  stock_index <- assessdatr %>%
+    select(Year, `SSB (mt)`) %>%
+    mutate(stock_index = scale(`SSB (mt)`)[,1]) %>%
+    select(-`SSB (mt)`) %>%
+    rename(year = Year) %>%
+    expand_grid(pred_seas, .) %>%
+    ungroup()
+}else{
+  stock_index <- herringdatr %>%
+    select(season, 
+           year = Year,
+           stock_index = Estimate_metric_tons) %>%
+    group_by(season) %>%
+    mutate(stock_index = scale(stock_index)[,1]) %>%
+    ungroup()
+}
 
-stock_index <- assessdatr %>%
-  select(Year, `SSB (mt)`) %>%
-  mutate(stock_index = scale(`SSB (mt)`)[,1]) %>%
-  select(-`SSB (mt)`) %>%
-  rename(year = Year) %>%
-  expand_grid(pred_seas, .) %>%
-  ungroup()
 
 plim <- max(c(diet_index$diet_index, overlap_index$overlap_index, stock_index$stock_index))
 nlim <- min(c(diet_index$diet_index, overlap_index$overlap_index, stock_index$stock_index))
@@ -52,24 +124,43 @@ diet_overlap_dat <- inner_join(diet_index, overlap_index, by = c("season", "pred
   group_by(season, predator) %>%
   mutate(Correlation = cor(`Diet index`, `Overlap index`))
 
-
-# Diet index vs assessment index
-diet_stock_dat <- inner_join(diet_index, stock_index, by = c("season", "predator", "year")) %>%
-  rename("Diet index" = diet_index,
-         "Assessment index" = stock_index) %>%
-  mutate(predator = str_to_sentence(predator),
-         season = str_to_sentence(season)) %>%
-  group_by(season, predator) %>%
-  mutate(Correlation = cor(`Diet index`, `Assessment index`))
-
-# Overlap index vs assessment index
-overlap_stock_dat <- inner_join(overlap_index, stock_index, by = c("season", "predator", "year")) %>%
-  rename("Overlap index" = overlap_index,
-         "Assessment index" = stock_index) %>%
-  mutate(predator = str_to_sentence(predator),
-         season = str_to_sentence(season)) %>%
-  group_by(season, predator) %>%
-  mutate(Correlation = cor(`Overlap index`, `Assessment index`))
+if(use_assessment == TRUE){
+  # Diet index vs assessment index
+  diet_stock_dat <- inner_join(diet_index, stock_index, by = c("season", "predator", "year")) %>%
+    rename("Diet index" = diet_index,
+           "Assessment index" = stock_index) %>%
+    mutate(predator = str_to_sentence(predator),
+           season = str_to_sentence(season)) %>%
+    group_by(season, predator) %>%
+    mutate(Correlation = cor(`Diet index`, `Assessment index`))
+  
+  # Overlap index vs assessment index
+  overlap_stock_dat <- inner_join(overlap_index, stock_index, by = c("season", "predator", "year")) %>%
+    rename("Overlap index" = overlap_index,
+           "Assessment index" = stock_index) %>%
+    mutate(predator = str_to_sentence(predator),
+           season = str_to_sentence(season)) %>%
+    group_by(season, predator) %>%
+    mutate(Correlation = cor(`Overlap index`, `Assessment index`))
+}else{
+  # Diet index vs herring index
+  diet_stock_dat <- inner_join(diet_index, stock_index, by = c("season", "year")) %>%
+    rename("Diet index" = diet_index,
+           "Assessment index" = stock_index) %>%
+    mutate(predator = str_to_sentence(predator),
+           season = str_to_sentence(season)) %>%
+    group_by(season, predator) %>%
+    mutate(Correlation = cor(`Diet index`, `Assessment index`))
+  
+  # Overlap index vs herring index
+  overlap_stock_dat <- inner_join(overlap_index, stock_index, by = c("season", "year")) %>%
+    rename("Overlap index" = overlap_index,
+           "Assessment index" = stock_index) %>%
+    mutate(predator = str_to_sentence(predator),
+           season = str_to_sentence(season)) %>%
+    group_by(season, predator) %>%
+    mutate(Correlation = cor(`Overlap index`, `Assessment index`))
+}
 
 
 # 2. Calculate correlations --------------------------------------------------
