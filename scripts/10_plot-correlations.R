4476e52ec4666e662285f0bc41a2f871cb6b10c8
Elizabeth Ng <elng@uw.edu>
2020-05-08 14:33:57 -0700

    Update correlation plot script to work for external overlap index
    with standard error. Roll this back if for some reason I want to go back/revert to using my manual calculation without SEs for overlap.

--- scripts/10_plot-correlations.R
+++ scripts/10_plot-correlations.R
@@ -16,66 +16,72 @@ assessment_cv <- 0.02
 
 # Load data and format for combining
 dietindexr <- read_rds(here::here("output", "index_diet.rds"))
-overlapindexr <- read_rds(here::here("output", "index_overlap.rds"))
+overlapindexr <- read_rds(here("output", "EXTERNAL_schoeners_D.rds")) %>% 
+  rename(season = Season, year = Year, overlap = Estimate, overlap_se = `Std. Error`) %>%
+  mutate(predator = tolower(predator), season = tolower(season)) # read_rds(here::here("output", "index_overlap.rds"))
 assessdatr <- readxl::read_xlsx(here("data", "raw", "TimeSeries.xlsx"))
 
-# add my Atlantic herring index
-herringdatr <- readr::read_rds(here::here("output", "top_final_trawl.rds")) %>%
-  filter(species == "atlantic herring") %>%
-  select(season, species, output) %>%
-  mutate(output = map(output, "result")) %>%
-  mutate(output = map(output, "index")) %>%
-  unnest(output)
-  
 
 
 # Plot indices as time-series ---------------------------------------------
-abundance_indices <- pivot_longer(assessdatr, cols = -Year) %>%
-  filter(name %in% c("SSB (mt)",
-                     "Jan.1 Biomass (mt)",
-                     "Catch (mt)")) %>%
-  mutate(source = "assessment") %>%
-  bind_rows(herringdatr %>%
-              mutate(source = "st index") %>%
-              select(Year, 
-                     source, 
-                     name = season, 
-                     value = Estimate_metric_tons)) %>%
-  bind_rows(dietindexr %>%
-              mutate(name = paste(predator, season),
-                     source = "diet index") %>%
-              select(Year = year,
-                     source,
-                     name,
-                     value = density)) %>%
-  group_by(name) %>%
-  mutate(value_z = scale(value)[,1])
+if(use_assessment == FALSE){
+  # add my Atlantic herring index
+  herringdatr <- readr::read_rds(here::here("output", "top_final_trawl.rds")) %>%
+    filter(species == "atlantic herring") %>%
+    select(season, species, output) %>%
+    mutate(output = map(output, "result")) %>%
+    mutate(output = map(output, "index")) %>%
+    unnest(output)
+  
+  abundance_indices <- pivot_longer(assessdatr, cols = -Year) %>%
+    filter(name %in% c("SSB (mt)",
+                       "Jan.1 Biomass (mt)",
+                       "Catch (mt)")) %>%
+    mutate(source = "assessment") %>%
+    bind_rows(herringdatr %>%
+                mutate(source = "st index") %>%
+                select(Year, 
+                       source, 
+                       name = season, 
+                       value = Estimate_metric_tons)) %>%
+    bind_rows(dietindexr %>%
+                mutate(name = paste(predator, season),
+                       source = "diet index") %>%
+                select(Year = year,
+                       source,
+                       name,
+                       value = density)) %>%
+    group_by(name) %>%
+    mutate(value_z = scale(value)[,1])
+  
+  ggplot(abundance_indices, aes(x = Year, y = value_z, group = name, color = source)) +
+    geom_point() +
+    geom_line(size = 1, alpha = 0.5) +
+    scale_color_manual(values = c(
+      scales::muted("blue", l = 50, c = 100), 
+      "grey",
+      scales::muted("red", l = 50, c = 100))) +
+    theme_bw()
+  
+  mean_diets <- filter(abundance_indices, source == "diet index") %>%
+    group_by(Year, source) %>%
+    summarize(value_z = mean(value_z, na.rm = TRUE)) %>%
+    mutate(name = "average diets") %>%
+    bind_rows(abundance_indices %>%
+                filter(source != "diet index"))
+  
+  ggplot(mean_diets, aes(x = Year, y = value_z, group = name, color = source)) +
+    geom_point() +
+    geom_line() +
+    scale_color_manual(values = c(
+      scales::muted("blue", l = 50, c = 100), 
+      "grey",
+      scales::muted("red", l = 50, c = 100))) +
+    theme_bw()
+  ggsave(here("output", "plots", "index-ts.pdf"), width = 8.5, height = 4)
+}
 
-ggplot(abundance_indices, aes(x = Year, y = value_z, group = name, color = source)) +
-  geom_point() +
-  geom_line(size = 1, alpha = 0.5) +
-  scale_color_manual(values = c(
-    scales::muted("blue", l = 50, c = 100), 
-    "grey",
-    scales::muted("red", l = 50, c = 100))) +
-  theme_bw()
 
-mean_diets <- filter(abundance_indices, source == "diet index") %>%
-  group_by(Year, source) %>%
-  summarize(value_z = mean(value_z, na.rm = TRUE)) %>%
-  mutate(name = "average diets") %>%
-  bind_rows(abundance_indices %>%
-              filter(source != "diet index"))
-
-ggplot(mean_diets, aes(x = Year, y = value_z, group = name, color = source)) +
-  geom_point() +
-  geom_line() +
-  scale_color_manual(values = c(
-    scales::muted("blue", l = 50, c = 100), 
-    "grey",
-    scales::muted("red", l = 50, c = 100))) +
-  theme_bw()
-ggsave(here("output", "plots", "index-ts.pdf"), width = 8.5, height = 4)
 
 # 0. Scale and rename -----------------------------------------------------
 diet_index <- dietindexr %>%
@@ -87,10 +93,10 @@ diet_index <- dietindexr %>%
   ungroup()
 
 overlap_index <- overlapindexr %>%
-  mutate(cv_overlap = overlap_cv) %>%
+  mutate(cv_overlap = overlap_se/overlap) %>%
   group_by(season, predator) %>%
-  mutate(overlap_index = scale(`Overlap index`)[,1]) %>%
-  select(-`Overlap index`) %>%
+  mutate(overlap_index = scale(overlap)[,1]) %>%
+  select(-overlap, -lcb, -ucb) %>%
   ungroup()
 
 if(use_assessment == TRUE){
@@ -316,3 +322,4 @@ ggplot(diet_overlap, aes(x = year, y = `Diet index`, color = `Overlap index`)) +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         strip.background = element_blank())
+
