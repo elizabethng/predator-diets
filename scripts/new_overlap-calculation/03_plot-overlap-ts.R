57ec2c681b473159d51eabd39f215013e6bb9080
Elizabeth Ng <elng@uw.edu>
2020-11-29 14:04:57 -0700

    Process output and make plot
    Noticed that error bounds are the same, back to bootstrap function, why did they not vary from median?

--- scripts/new_overlap-calculation/03_plot-overlap-ts.R
+++ scripts/new_overlap-calculation/03_plot-overlap-ts.R
@@ -4,24 +4,59 @@ library("tidyverse")
 library("here")
 
 
-# Load files --------------------------------------------------------------
-# Get file names
-# I don't think I'll be able to read them all
-rawres <- tibble(filenames = here("scripts", "new_overlap-calculation", "output") %>%
-                   dir()) %>%
-  separate(filenames, into = c("predator", "season"), sep = "_", remove = FALSE) %>%
-  mutate(season = gsub(".rds", "", season)) %>%
-  slice(1) %>%
-  mutate(results = list(read_rds(
-    here("scripts", "new_overlap-calculation", "output", filenames))
-  ))
-rawres
-
+# Functions ---------------------------------------------------------------
 # Function to read rds and extract year effect only (don't need locations for year)
 get_year_effects <- function(path){
-  res <- read_rds(here("scripts", "new_overlap-calculation", "output", path))
+  res <- read_rds(path)
   out <- res %>%
     select(-data) %>%
     mutate(results = map(results, "annual")) %>%
     unnest(results)
+  return(out)
 }
+# jj <- get_year_effects(here("scripts", "new_overlap-calculation", "output", "atlantic cod_fall.rds"))
+
+
+# Load results --------------------------------------------------------------
+rawres <- here("scripts", "new_overlap-calculation", "output") %>% 
+  dir() %>%
+  tibble() %>%
+  rename(filenames = ".") %>%
+  rowwise() %>%
+  mutate(
+    results = list(get_year_effects(here("scripts", "new_overlap-calculation", "output", filenames)))
+  )
+  
+
+# Format and plot ---------------------------------------------------------
+plotdat <- rawres %>%
+  unnest(results) %>%
+  select(-filenames) %>%
+  mutate(
+    season = str_to_sentence(season),
+    predator = str_to_sentence(predator)
+  ) %>%
+  rename(
+    Season = season
+  )
+  
+ggplot(plotdat, aes(x = year, y = range_overlap, color = Season, fill = Season)) +
+  geom_point() +
+  geom_line() +
+  geom_ribbon(
+    aes(ymin = lcb, ymax = ucb, fill = Season), alpha = 0.3, color = NA
+    ) +
+  scale_color_manual(
+    aesthetics = c("color", "fill"),
+    values = c(scales::muted("blue", l = 50, c = 100), 
+               scales::muted("red", l = 50, c = 100))
+    ) +
+  ylab("Range overlap") +
+  facet_wrap(~predator) +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank())
+
+
