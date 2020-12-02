5eaae81ca4a8419cf28728e7bb56cd9e223ca529
Elizabeth Ng <elng@uw.edu>
2020-12-02 05:27:28 -0700

    Order parameters and save output tables

--- scripts/12_make-parameter-estimate-appendix.R
+++ scripts/12_make-parameter-estimate-appendix.R
@@ -24,6 +24,7 @@ format_sd_report <- function(SD){
 
 # Load data --------------------------------------------------------------
 # Read in all opt output, which contain SD reports
+# Remove anisotropy parameters becasue I don't discuss them in the paper
 optdat <- tibble(
   locs = here("output", "diagnostics") %>%
     dir()
@@ -39,8 +40,54 @@ optdat <- tibble(
   separate(locs, c("model", "season", "species"), sep = "_") %>%
   mutate(species = gsub("-", " ", species)) %>%
   select(-path) %>%
-  unnest(data)
+  unnest(data) %>%
+  filter(str_detect(parameter, "input", negate = TRUE))
 
 
 # Format table for output -------------------------------------------------
+# [ ] order is predator name, then spring/fall
+# [-] add symbol column --> might just do this manually
+# [ ] update column names
+
+param_order <- tribble(
+    ~ parameter,    ~ order,
+    "L_epsilon1_z", 6,
+    "L_epsilon2_z", 7,
+    "L_omega1_z",   4,
+    "L_omega2_z",   5,
+    "logkappa1",    2,
+    "logkappa2",    3,
+    "logSigmaM",    1
+    ) %>%
+  mutate(new_name = paste(order, parameter, sep = "_")) %>%
+  arrange(new_name) %>%
+  select(-order)
+
+formdat <- optdat %>%
+  left_join(param_order, by = "parameter") %>%
+  select(model, 
+         Species = species, 
+         Season = season, 
+         Parameter = new_name, 
+         Estimate = estimate, 
+         SE = est_se, 
+         Gradient = gradient) %>%
+  arrange(Species, desc(Season), Parameter) %>%
+  mutate(
+    Species = str_to_sentence(Species),
+    Season = str_to_sentence(Season)
+  )
+  
+# Split tables and save ---------------------------------------------------
+# Diet table
+diettab <- formdat %>%
+  filter(model == "diet") %>%
+  select(-model)
+
+trawltab <- formdat %>%
+  filter(model == "trawl") %>%
+  select(-model)
+
+write_csv(diettab, here("output", "tables", "param_ests_diet.csv"))
+write_csv(trawltab, here("output", "tables", "param_ests_trawl.csv"))
 
